e2bba0311799cafed939a5dd5c95b23c19e794ea
Lucas Sledsens <lucas_sledsens@hotmail.com>
2021-08-30 16:06:49 +0200

    rm testing, rm unused dependencies and add barrel index file

--- src/components/side-navigation/test/e2e/components/vl-side-navigation-toggle.js
+++
@@ -1,9 +0,0 @@
-const {VlElement} = require('vl-ui-core').Test;
-
-class VlSideNavigationItem extends VlElement {
-  async isActive() {
-    return this.hasClass('js-vl-scrollspy-active');
-  }
-}
-
-module.exports = VlSideNavigationItem;
---
+++ src/index.js
@@ -0,0 +1,29 @@
+export { VlAccessibility } from "./components/accessibility-statement";
+export { VlAccordion } from "./components/accordion";
+export { VlBody } from "./components/body";
+export { VlButton, VlLinkButton } from "./components/button";
+export { VlContactCard } from "./components/contact-card";
+export { VlFunctionalHeader } from "./components/functional-header";
+export { VlRegion, VlLayout, VlGrid, VlColumn } from "./components/grid";
+export { VlIcon, VlIconWrapper } from "./components/icon";
+export { VlInfoblock } from "./components/infoblock";
+export { VlIntroduction } from "./components/introduction";
+export { VlLink, VlButtonLink } from "./components/link";
+export {
+  VlProperties,
+  VlPropertiesColumn,
+  VlPropertiesList,
+  VlPropertyTerm,
+  VlPropertyValue,
+} from "./components/properties";
+export {
+  VlSideNavigation,
+  VlSideNavigationReference,
+  VlSideNavigationTitle,
+  VlSideNavigationContent,
+  VlSideNavigationGroup,
+  VlSideNavigationItem,
+  VlSideNavigationToggle,
+} from "./components/side-navigation";
+export { VlH1, VlH2, VlH3, VlH4, VlH5, VlH6 } from "./components/titles";
+export { VlTypography } from "./components/typography";
