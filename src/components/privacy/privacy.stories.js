ff9e9dc875d1e51ee7f9a7ed5245d61e03597aaf
Lucas Sledsens <lucas_sledsens@hotmail.com>
2021-09-01 10:37:05 +0200

    add components and refactor privcacy component

---
+++ src/components/privacy/privacy.stories.js
@@ -0,0 +1,36 @@
+import { html } from "lit-html";
+import "../privacy";
+
+export default {
+  title: "custom-elements/vl-privacy",
+  args: {
+    version: "1.0.0",
+    date: "3 maart 2021",
+  },
+  argTypes: {
+    version: {
+      name: "data-vl-version",
+      type: { summary: "string" },
+      description: "Attribuut wordt gebruikt om de pagina versie aan te geven.",
+      table: {
+        defaultValue: { summary: '"1.0.0"' },
+      },
+    },
+    date: {
+      name: "data-vl-date",
+      type: { summary: "string" },
+      description:
+        "Attribuut wordt gebruikt om aan te geven op welke datum deze pagina opgesteld werd.",
+      table: {
+        defaultValue: { summary: '"3 maart 2021"' },
+      },
+    },
+  },
+};
+
+export const Default = ({ version, date }) => {
+  return html`<vl-privacy
+    data-vl-version=${version}
+    data-vl-date=${date}
+  ></vl-privacy>`;
+};
