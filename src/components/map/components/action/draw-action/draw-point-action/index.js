48180a027ae34c95a01ff7264bde33ca14dfd971
Gert-Jan Meire <gertjan.meire@gmail.com>
2021-09-17 10:48:12 +0200

    UIG-1414: Renamed components.

---
+++ src/components/map/components/action/draw-action/draw-point-action/index.js
@@ -0,0 +1,27 @@
+import { define } from "../../../../../../utils/core";
+import { VlMapDrawAction } from "../../draw-action";
+import { VlDrawAction, OlGeometryType } from "../../../../mapactions";
+
+/**
+ * VlMapDrawPointAction
+ * @class
+ * @classdesc De kaart teken actie component.
+ *
+ * @extends VlMapDrawAction
+ *
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-map/releases/latest|Release notes}
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-map/issues|Issues}
+ * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-map-draw-actions.html|Demo}
+ */
+export class VlMapDrawPointAction extends VlMapDrawAction {
+  _createAction(layer) {
+    return new VlDrawAction(
+      layer,
+      OlGeometryType.POINT,
+      this._callback,
+      this.__drawOptions
+    );
+  }
+}
+
+define("vl-map-draw-point-action", VlMapDrawPointAction);
