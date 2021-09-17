3fa9b2e9727bcf5779f459854ecab0c564967645
Lucas Sledsens <lucas_sledsens@hotmail.com>
2021-09-17 12:58:17 +0200

    add body story

--- src/components/body/body.stories.js
+++ src/components/body/body.stories.js
@@ -3,6 +3,18 @@ import "../body";
 
 export default {
   title: "native-elements/vl-body",
+  parameters: {
+    controls: { hideNoControlsWarning: true },
+  },
 };
 
-export const Default = () => html``;
+export const Default = () =>
+  html`
+    <!-- just shows usage -->
+    <body is="vl-body">
+      <p>
+        By using the native vl-body element incl. styling, a minimum of
+        corporate identity will be loaded.
+      </p>
+    </body>
+  `;
