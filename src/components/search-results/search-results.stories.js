b57344c0686745bdcda6d406ab39dc410fe699f1
Gert-Jan Meire <gertjan.meire@gmail.com>
2021-11-18 13:03:05 +0100

    UIG-1461: Fixed issue.

--- src/components/search-results/search-results.stories.js
+++ src/components/search-results/search-results.stories.js
@@ -4,7 +4,7 @@ import styles from "../search-results/styles.scss";
 import { stylesheet, docsIntro } from "../../../.storybook/utils.js";
 
 export default {
-  title: "custom-elements/vl-search-results",
+  title: "native-elements/vl-search-results",
   controls: { hideNoControlsWarning: true },
   decorators: [(story) => html`${stylesheet(styles)}${story()}`],
   parameters: {
