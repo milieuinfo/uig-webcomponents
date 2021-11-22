51567c5784543744348d461b39996f1523f4105c
Gert-Jan Meire <gertjan.meire@gmail.com>
2021-11-22 13:04:22 +0100

    UIG-1461: Split up form stories.

--- src/components/form/form.stories.js
+++ src/components/form/form.stories.js
@@ -21,6 +21,7 @@ export default {
       `,
   ],
   parameters: {
+    controls: { hideNoControlsWarning: true },
     docs: {
       description: {
         component: docsIntro({
@@ -35,7 +36,45 @@ export default {
   argTypes,
 };
 
-const Template = ({ validate }) => html`
+export const Default = () => html`
+  <div style="max-width: 800px">
+    <form is="vl-form">
+      <div is="vl-form-grid" data-vl-is-stacked>
+        <div is="vl-form-column" data-vl-size="3">
+          <label is="vl-form-label" for="name" data-vl-block>
+            Naam
+            <span is="vl-form-annotation-span">(verplicht)</span>
+          </label>
+        </div>
+        <div is="vl-form-column" data-vl-size="9">
+          <input name="name" autocomplete="name" is="vl-input-field" data-vl-block />
+        </div>
+
+        <div is="vl-form-column" data-vl-size="3">
+          <label is="vl-form-label" for="firstname" data-vl-block>
+            Voornaam
+            <span is="vl-form-annotation-span">(verplicht)</span>
+          </label>
+        </div>
+        <div is="vl-form-column" data-vl-size="9">
+          <input name="firstname" autocomplete="firstname" is="vl-input-field" data-vl-block />
+        </div>
+
+        <div is="vl-form-column" data-vl-size="9" data-vl-push="3">
+          <div is="vl-action-group">
+            <button is="vl-button" type="submit">Versturen</button>
+            <a is="vl-link" href="#">
+              <span is="vl-icon" data-vl-icon="cross" data-vl-before></span>
+              Annuleren
+            </a>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+`;
+
+export const WithValidation = ({ validate }) => html`
   <div style="max-width: 800px">
     <form is="vl-form" ?data-vl-validate=${validate}>
       <div is="vl-form-grid" data-vl-is-stacked>
@@ -90,10 +129,3 @@ const Template = ({ validate }) => html`
     </form>
   </div>
 `;
-
-export const Default = Template.bind({});
-export const WithValidation = Template.bind({});
-
-Default.args = {
-  validate: false,
-};
