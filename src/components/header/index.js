26b4c98939643136125f1e54de23a132f36ff6a9
Lucas Sledsens <lucas_sledsens@hotmail.com>
2021-11-23 10:06:17 +0100

    reflect properties

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,83 +1,70 @@
-import { LitElement } from "lit";
-import { awaitScript } from "../../utils/core";
+import { LitElement } from 'lit';
+import { awaitScript } from '../../utils/core';
 
 awaitScript(
-  "vl-header-client",
-  "https://prod.widgets.burgerprofiel.vlaanderen.be/api/v1/node_modules/@govflanders/vl-widget-polyfill/dist/index.js"
+  'vl-header-client',
+  'https://prod.widgets.burgerprofiel.vlaanderen.be/api/v1/node_modules/@govflanders/vl-widget-polyfill/dist/index.js',
 )
   .then(() => {
     awaitScript(
-      "vl-header-polyfill",
-      "https://prod.widgets.burgerprofiel.vlaanderen.be/api/v1/node_modules/@govflanders/vl-widget-client/dist/index.js"
+      'vl-header-polyfill',
+      'https://prod.widgets.burgerprofiel.vlaanderen.be/api/v1/node_modules/@govflanders/vl-widget-client/dist/index.js',
     ).finally(() => {
-      customElements.define("vl-header", VlHeader);
+      customElements.define('vl-header', VlHeader);
     });
   })
   .catch(() => {
-    customElements.define("vl-header", VlHeader);
+    customElements.define('vl-header', VlHeader);
   });
-
-const props = {
-  development: "data-vl-development",
-  identifier: "data-vl-identifier",
-};
-const { development, identifier } = props;
 
 export class VlHeader extends LitElement {
   static get properties() {
     return {
-      [identifier]: { type: String },
-      [development]: { type: Boolean },
+      identifier: { type: String, attribute: 'data-vl-identifier', reflect: true },
+      development: { type: Boolean, attribute: 'data-vl-development', reflect: true },
     };
-  }
-
-  constructor() {
-    super();
   }
 
   injectHeader() {
     const vlBody = document.querySelector('[is="vl-body"]');
-    (vlBody || document.body).insertAdjacentHTML(
-      "afterbegin",
-      '<div id="header"></div>'
-    );
+    (vlBody || document.body).insertAdjacentHTML('afterbegin', '<div id="header"></div>');
   }
 
   vlwHeader() {
-    return document.querySelector("div[class=vlw__header]");
+    return document.querySelector('div[class=vlw__header]');
   }
 
   header() {
-    return document.querySelector("#header");
+    return document.querySelector('#header');
   }
 
   async __isUserAuthenticated() {
-    const response = await fetch("/LoggedInUser");
+    const response = await fetch('/LoggedInUser');
     return response.status === 200;
   }
 
   loadWidget() {
-    const widgetUrl = this[development]
-      ? `https://tni.widgets.burgerprofiel.dev-vlaanderen.be/api/v1/widget/${this[identifier]}`
-      : `https://prod.widgets.burgerprofiel.vlaanderen.be/api/v1/widget/${this[identifier]}`;
+    const widgetUrl = this.development
+      ? `https://tni.widgets.burgerprofiel.dev-vlaanderen.be/api/v1/widget/${this.identifier}`
+      : `https://prod.widgets.burgerprofiel.vlaanderen.be/api/v1/widget/${this.identifier}`;
 
     window.vl.widget.client
       .bootstrap(widgetUrl)
       .then((widget) => {
         this.injectHeader();
-        widget.setMountElement(document.getElementById("header"));
+        widget.setMountElement(document.getElementById('header'));
         widget.mount().catch((e) => console.error(e));
         return widget;
       })
       .then((widget) => {
-        widget.getExtension("citizen_profile.session").then(async (session) => {
+        widget.getExtension('citizen_profile.session').then(async (session) => {
           session.configure({
             active: await this.__isUserAuthenticated(),
             endpoints: {
-              loginUrl: "/aanmelden",
-              loginRedirectUrl: "/",
-              logoutUrl: "/afgemeld",
-              switchCapacityUrl: "/wissel_organisatie",
+              loginUrl: '/aanmelden',
+              loginRedirectUrl: '/',
+              logoutUrl: '/afgemeld',
+              switchCapacityUrl: '/wissel_organisatie',
             },
           });
         });
