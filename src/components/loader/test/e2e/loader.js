85ce39915eeb545bf1eeee5bda446f4ff439369a
Gert-Jan Meire <gertjan.meire@gmail.com>
2021-11-25 10:17:15 +0100

    UIG-1548: Added loader component.

---
+++ src/components/loader/test/e2e/loader.js
@@ -0,0 +1,27 @@
+import { VlElement, By } from '../../../../utils/test';
+
+export default class VlLoader extends VlElement {
+  async isLight() {
+    return this.hasAttribute('data-vl-light');
+  }
+
+  async isSingle() {
+    return this.hasAttribute('data-vl-single');
+  }
+
+  async getText() {
+    const slot = await this._getSlotElement();
+    const element = await new VlElement(this.driver, slot);
+    return element.getText();
+  }
+
+  async slotElements() {
+    const slot = await this._getSlotElement();
+    return this.getAssignedElements(slot);
+  }
+
+  async _getSlotElement() {
+    return this.shadowRoot.findElement(By.css('slot'));
+  }
+}
+
