24fe87dad2c06a0bfb4e7558c73880d4158a83e0
Lucas Sledsens <lucas_sledsens@hotmail.com>
2021-11-29 15:16:15 +0100

    form e2e

--- src/components/form/test/e2e/form.spec.js
+++ src/components/form/test/e2e/form.spec.js
@@ -1,27 +1,27 @@
-const { VlInputField } = require('vl-ui-input-field').Test;
-
+import { VlInputField } from '../../../input-field/test/e2e/input-field';
+import { VlForm } from './form';
 import { assert, getDriver, config, By } from '../../../../utils/test';
+
+const { sbUrl } = config;
+const defaultUrl = `${sbUrl}?id=native-elements-vl-form--default`;
+const selector = 'form[is="vl-form"]';
 
 describe('vl-form', async () => {
   let driver;
-  let vlFormPage;
 
-  before(() => {
+  beforeEach(() => {
     driver = getDriver();
-    vlFormPage = new VlFormPage(driver);
-    return vlFormPage.load();
-  });
-
-  it('WCAG', async () => {
-    await assert.eventually.isFalse(vlFormPage.hasWcagIssues());
+    driver.manage().window().maximize();
   });
 
   it('als gebruiker kan ik een formulier submitten zonder dat de URL aangepast wordt', async () => {
-    const form = await vlFormPage.getForm();
-    const nameInput = await new VlInputField(driver, await form.findElement(By.css('#name')));
-    await nameInput.setValue('Jos');
+    await driver.get(defaultUrl);
+    const form = await new VlForm(driver, selector);
+    const input = await new VlInputField(driver, await form.findElement(By.css('input[name="name"]')));
+    await input.setValue('foo');
     await form.submit();
     const url = await driver.getCurrentUrl();
-    assert.isTrue(url.endsWith('?no-header=true&no-footer=true'));
+    console.log({ url });
+    assert.isTrue(url.endsWith('&viewMode=story'));
   });
 });
