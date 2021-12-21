b1d590117beba73fe4cc219bd00f77f866434cf8
Lucas Sledsens <lucas_sledsens@hotmail.com>
2021-12-21 16:47:17 +0100

    infoblock e2e

--- src/components/infoblock/infoblock.stories.js
+++ src/components/infoblock/infoblock.stories.js
@@ -1,76 +1,64 @@
-import { html } from "lit-html";
-import "../infoblock";
-import { docsIntro } from "../../../.storybook/utils.js";
+import { html } from 'lit-html';
+import '../infoblock';
+import { ifDefined } from 'lit-html/directives/if-defined';
+import { docsIntro } from '../../../.storybook/utils.js';
 
 export default {
-  title: "custom-elements/vl-infoblock",
+  title: 'custom-elements/vl-infoblock',
   parameters: {
     docs: {
       description: {
         component: docsIntro({
-          root: "infoblock",
-          intro:
-            "Gebruik de infoblock om een sectie met een nieuwe content te starten.",
+          root: 'infoblock',
+          intro: 'Gebruik de infoblock om een sectie met een nieuwe content te starten.',
         }),
       },
     },
   },
   argTypes: {
     type: {
-      name: "data-vl-type",
-      type: "select",
-      options: [
-        "contact",
-        "publications",
-        "faq",
-        "news",
-        "timeline",
-        "question",
-      ],
-      description:
-        "Er kan een vast icoon gekozen worden (contact, publications, faq, news, timeline, question).",
+      name: 'data-vl-type',
+      type: 'select',
+      options: ['contact', 'publications', 'faq', 'news', 'timeline', 'question'],
+      description: 'Er kan een vast icoon gekozen worden (contact, publications, faq, news, timeline, question).',
       table: {
-        type: { summary: "string" },
-        defaultValue: { summary: "" },
+        type: { summary: 'string' },
+        defaultValue: { summary: '' },
       },
     },
     title: {
-      name: "data-vl-title",
-      type: { summary: "string" },
-      description:
-        "Attribuut dat wordt gebruikt om de titel van de infoblock te zetten.",
+      name: 'data-vl-title',
+      type: { summary: 'string' },
+      description: 'Attribuut dat wordt gebruikt om de titel van de infoblock te zetten.',
       table: {
-        defaultValue: { summary: "" },
+        defaultValue: { summary: '' },
       },
     },
     icon: {
-      name: "data-vl-icon",
-      type: "select",
-      options: ["calendar", "programming-bug", "key"],
+      name: 'data-vl-icon',
+      type: 'select',
+      options: ['calendar', 'programming-bug', 'key'],
       description:
-        "Attribuut dat wordt gebruikt om een icoon vooraan aan de titel toe te voegen. Het icoon kan gekozen worden uit de lijst op https://overheid.vlaanderen.be/webuniversum/v3/documentation/atoms/vl-ui-icon.",
+        'Attribuut dat wordt gebruikt om een icoon vooraan aan de titel toe te voegen. Het icoon kan gekozen worden uit de lijst op https://overheid.vlaanderen.be/webuniversum/v3/documentation/atoms/vl-ui-icon.',
       control: {
         disable: true,
       },
       table: {
-        type: { summary: "string" },
-        defaultValue: { summary: "" },
+        type: { summary: 'string' },
+        defaultValue: { summary: '' },
       },
     },
     content: {
-      name: "content (for demo purposes)",
-      type: { summary: "string" },
+      name: 'content (for demo purposes)',
+      type: { summary: 'string' },
     },
   },
 };
 
-const Template = ({ title, content, type, icon }) => html`<vl-infoblock
-  data-vl-title=${title}
-  data-vl-type=${type}
-  data-vl-icon=${icon}
->
-  ${content}
-</vl-infoblock>`;
+const Template = ({ title, content, type, icon }) =>
+  html`<vl-infoblock data-vl-title=${title} data-vl-type=${ifDefined(type)} data-vl-icon=${icon}>
+    ${content}
+  </vl-infoblock>`;
 
 export const Contact = Template.bind({});
 export const Publications = Template.bind({});
@@ -81,52 +69,45 @@ export const Question = Template.bind({});
 export const CustomIcon = Template.bind({});
 
 Contact.args = {
-  title: "Contactenlijst",
-  content:
-    "Hieronder bevindt zich een overzicht van al uw contacten binnen de Vlaamse Overheid.",
-  type: "contact",
+  title: 'Contactenlijst',
+  content: 'Hieronder bevindt zich een overzicht van al uw contacten binnen de Vlaamse Overheid.',
+  type: 'contact',
 };
 
 Publications.args = {
-  title: "Contracten",
-  content:
-    "Hieronder bevindt zich een overzicht van al uw contracten binnen de Vlaamse Overheid",
-  type: "publications",
+  title: 'Contracten',
+  content: 'Hieronder bevindt zich een overzicht van al uw contracten binnen de Vlaamse Overheid',
+  type: 'publications',
 };
 
 FAQ.args = {
-  title: "Veelgestelde vragen",
-  content:
-    "Hieronder bevindt zich een overzicht van alle veelgestelde vragen binnen de Vlaamse Overheid.",
-  type: "faq",
+  title: 'Veelgestelde vragen',
+  content: 'Hieronder bevindt zich een overzicht van alle veelgestelde vragen binnen de Vlaamse Overheid.',
+  type: 'faq',
 };
 
 News.args = {
-  title: "Nieuwsberichten",
-  content:
-    "Hieronder bevindt zich een overzicht van alle nieuwsberichten binnen de Vlaamse Overheid.",
-  type: "news",
+  title: 'Nieuwsberichten',
+  content: 'Hieronder bevindt zich een overzicht van alle nieuwsberichten binnen de Vlaamse Overheid.',
+  type: 'news',
 };
 
 Timeline.args = {
-  title: "Archief",
-  content:
-    "Hieronder bevindt zicht een overzicht van alle gearchiveerde berichten binnen de Vlaamse Overheid.",
-  type: "timeline",
+  title: 'Archief',
+  content: 'Hieronder bevindt zicht een overzicht van alle gearchiveerde berichten binnen de Vlaamse Overheid.',
+  type: 'timeline',
 };
 
 Question.args = {
-  title: "Overheidsdiensten",
-  content:
-    "Hieronder bevindt zicht een overzicht van alle overheidsdiensten binnen de Vlaamse Overheid.",
-  type: "question",
+  title: 'Overheidsdiensten',
+  content: 'Hieronder bevindt zicht een overzicht van alle overheidsdiensten binnen de Vlaamse Overheid.',
+  type: 'question',
 };
 
 CustomIcon.args = {
-  title: "Evenementen",
-  content:
-    "Hieronder bevindt zicht een overzicht van alle evenementen binnen de Vlaamse Overheid.",
-  icon: "calendar",
+  title: 'Evenementen',
+  content: 'Hieronder bevindt zicht een overzicht van alle evenementen binnen de Vlaamse Overheid.',
+  icon: 'calendar',
 };
 
 CustomIcon.argTypes = {
@@ -145,14 +126,13 @@ export const WithSlotElements = ({ title, content, type }) => html`
 `;
 
 WithSlotElements.args = {
-  title: "Titel via slot",
-  content:
-    "Hieronder bevindt zich een overzicht van al uw contacten binnen de Vlaamse Overheid.",
-  type: "contact",
+  title: 'Titel via slot',
+  content: 'Hieronder bevindt zich een overzicht van al uw contacten binnen de Vlaamse Overheid.',
+  type: 'contact',
 };
 
 WithSlotElements.argTypes = {
   title: {
-    name: "title (slot)",
+    name: 'title (slot)',
   },
 };
