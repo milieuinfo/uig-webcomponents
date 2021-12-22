305b2aacab57165fce48fe074fb20d3a9754a82c
Gert-Jan Meire <gertjan.meire@gmail.com>
2021-12-22 11:03:29 +0100

    UIG-1545: Added known issues page.

--- docs/pages/introduction/index.js
+++ docs/pages/introduction/index.js
@@ -1,11 +1,11 @@
-import { html, LitElement, css, unsafeCSS } from "lit";
-import { wrap } from "../../wrap";
-import { removeStorybooksDefaultStyling } from "../../../.storybook/utils.js";
-import "../../../src/components/typography";
-import "../../../src/components/grid";
-import "../../../src/components/titles";
-import titleStyles from "../../../src/components/titles/styles.scss";
-import introStyles from "../../../src/components/introduction/styles.scss";
+import { html, LitElement, css, unsafeCSS } from 'lit';
+import { wrap } from '../../wrap';
+import { removeStorybooksDefaultStyling } from '../../../.storybook/utils.js';
+import '../../../src/components/typography';
+import '../../../src/components/grid';
+import '../../../src/components/titles';
+import titleStyles from '../../../src/components/titles/styles.scss';
+import introStyles from '../../../src/components/introduction/styles.scss';
 
 const block = ({ title, children }) => html`
   <div is="vl-column" data-vl-size="8">
@@ -33,66 +33,67 @@ export class Introduction extends LitElement {
 
   render() {
     return wrap({
-      title: "Introduction",
+      title: 'Introduction',
       intro:
-        "We offer a library of web components that is build upon the design system of Digitaal Vlaanderen. It is designed to work with any framework, or even without one. We guarantee consistency between the components.",
+        'We offer a library of web components that is build upon the design system of Digitaal Vlaanderen. It is designed to work with any framework, or even without one. We guarantee consistency between the components.',
       children: html`<section is="vl-region">
         <div is="vl-layout">
           <div is="vl-grid" data-vl-is-stacked>
             ${block({
-              title: "Getting started",
-              children: html`<p>
+        title: 'Getting started',
+        children: html`<p>
                 To get started, you need to install our package. To do so, run
-                <vl-typography
-                  ><code>npm install uig-webcomponents</code></vl-typography
+                <vl-typography><code>npm install uig-webcomponents</code></vl-typography
                 >. Once the installation is done, you are ready to go.
               </p>`,
-            })}
+      })}
             ${block({
-              title: "Usage",
-              children: html`
+        title: 'Usage',
+        children: html`
                 <p>
-                  You can now import and use our components in your project.
-                  Each component has a docs page, where you find what needs to
-                  be imported in order to use it.
+                  You can now import and use our components in your project. Each component has a docs page, where you
+                  find what needs to be imported in order to use it.
                 </p>
               `,
-            })}
+      })}
             ${block({
-              title: "Migrating from legacy packages",
-              children: html`
+        title: 'Migrating from legacy packages',
+        children: html`
                 <p>
-                  If you are migrating from the legacy packages to this package,
-                  we recommend to carefully read the docs and the changelog.
-                  Every breaking change across from the legacy package is
-                  mentioned in the changelog with a '&#x1F4A5;'. We use semantic
-                  versioning for our own changes.
+                  If you are migrating from the legacy packages to this package, we recommend to carefully read the docs
+                  and the changelog. Every breaking change across from the legacy package is mentioned in the changelog
+                  with a '&#x1F4A5;'. We use semantic versioning for our own changes.
                 </p>
               `,
-            })}
+      })}
             ${block({
-              title: "Getting in touch",
-              children: html`
+        title: 'Getting in touch',
+        children: html`
                 <vl-typography>
                   <p>
-                    We use our internal Rocket.Chat as our main
-                    communication-channel. We have two channels that each serve
-                    a specific purpose. Feel free to join the ones that are
-                    applicable to you.
+                    We use our internal Rocket.Chat as our main communication-channel. We have two channels that each
+                    serve a specific purpose. Feel free to join the ones that are applicable to you.
                   </p>
                   <ul>
+                    <li><code>#uig-development</code> for all development related questions and assistance.</li>
+                    <li><code>#uig-releases</code> to get notified when the package launches a new release.</li>
+                  </ul>
+                </vl-typography>
+              `,
+      })}
+            ${block({
+        title: 'Known issues',
+        children: html`
+                <vl-typography>
+                  <ul>
                     <li>
-                      <code>#uig-development</code> for all development related
-                      questions and assistance.
-                    </li>
-                    <li>
-                      <code>#uig-releases</code> to get notified when the
-                      package launches a new release.
+                      The <code>infotext</code> component has a console log error when this component is being used more
+                      than once on a page. This issue will be resolved in the future.
                     </li>
                   </ul>
                 </vl-typography>
               `,
-            })}
+      })}
           </div>
         </div>
       </section>`,
@@ -100,4 +101,4 @@ export class Introduction extends LitElement {
   }
 }
 
-customElements.define("docs-introduction", Introduction);
+customElements.define('docs-introduction', Introduction);
