fdd24778f85dee6b4cb27d86e516948d4ee0200d
Lucas Sledsens <lucas_sledsens@hotmail.com>
2022-01-28 13:45:32 +0100

    Revert "esm (#65)"
    This reverts commit c6bf12277c140285eb37270a9a064029fa7ada5c.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,12 +1,10 @@
-import path from 'path';
+const path = require("path");
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
-export default {
-  mode: 'development',
-  entry: './lib/index.js',
+module.exports = {
+  mode: "development",
+  entry: "./lib/index.js",
   output: {
-    path: path.resolve(__dirname, '../build'),
-    filename: 'index.js',
+    path: path.resolve(__dirname, "../build"),
+    filename: "index.js",
   },
 };
