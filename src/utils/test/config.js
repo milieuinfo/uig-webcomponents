e55ac9c6660fbb2e08db8de88539e5b3f1206b14
Sander Kleykens <sander.kleykens@avnu.be>
2022-02-09 12:11:25 +0100

    Introduceer "type": "module" in package.json (#69)
    * esm
    
    * Update build.js
    
    * Update build.js
    
    * Remove esm, add node ES module resolution and change e2e test setup to use ES module imports
    
    * esm terug toevoegen
    
    * update selenium-webdriver
    
    * try with explicit --require @babel/register
    
    * package-lock.json opnieuw proberen genereren
    
    * import from selenium-webdriver anders doen
    
    * import from selenium-webdriver anders doen
    
    * revert selenium-webdriver update
    
    * probeer require ipv import voor selenium-webdriver
    
    * Upgrade node versie
    
    * Import selenium webdriver
    
    * update chromedriver
    
    * bug rond yargs oplossen
    
    * Revert import van package.json in e2e setup.js
    
    * Refactor
    
    * Revert "Refactor"
    
    This reverts commit 6bb582f211fb92c376f8d1bff53dfe3da31ceae5.
    
    * Revert "Revert import van package.json in e2e setup.js"
    
    This reverts commit 56de808432099530b02b698a0af4245c3c8e5365.
    
    * refactor test config
    
    * default chrome
    
    * package json default export
    
    * 2.2.1-alpha.0
    
    * add 2.2.1 changelog
    
    Co-authored-by: Lucas Sledsens <lucas_sledsens@hotmail.com>
    Co-authored-by: bamboo <bamboo@milieuinfo.be>

--- src/utils/test/config.js
+++ src/utils/test/config.js
@@ -1,80 +1,32 @@
-const yargs = require('yargs').argv;
+import yargs from 'yargs';
 
-const macOS = 'OS X';
-const windows = 'Windows';
+const { argv } = yargs(process.argv);
 
-const macOsVersion = 'Catalina';
-const windowsVersion = '10';
-
-const chrome = 'chrome';
-const firefox = 'firefox';
-const edge = 'edge';
-const safari = 'safari';
-const opera = 'opera';
-
-const osName = () => {
-  switch (browserName()) {
-    case safari:
-      return macOS;
-    default:
-      return windows;
-  }
+const operatingSystems = {
+  mac: { name: 'OS X', version: 'Catalina' },
+  windows: { name: 'Windows', version: '10' },
 };
 
-const osVersion = () => {
-  switch (osName()) {
-    case macOS:
-      return macOsVersion;
-    default:
-      return windowsVersion;
-  }
-};
+const browsers = [
+  { name: 'chrome', isActive: argv.chrome, version: '85.0', os: operatingSystems.windows },
+  { name: 'firefox', isActive: argv.firefox, version: '83.0', os: operatingSystems.windows },
+  { name: 'edge', isActive: argv.edge, version: '86.0', os: operatingSystems.windows },
+  { name: 'safari', isActive: argv.safari, version: 'latest', os: operatingSystems.mac },
+  { name: 'opera', isActive: argv.opera, version: 'latest', os: operatingSystems.windows },
+];
 
-const browserName = () => {
-  if (process.argv || yargs) {
-    if (process.argv.includes(chrome) || yargs.chrome) {
-      return chrome;
-    }
-    if (process.argv.includes(firefox) || yargs.firefox) {
-      return firefox;
-    }
-    if (process.argv.includes(edge) || yargs.edge) {
-      return edge;
-    }
-    if (process.argv.includes(safari) || yargs.safari) {
-      return safari;
-    }
-    if (process.argv.includes(opera) || yargs.opera) {
-      return opera;
-    }
-    console.warn('Geen geldige browser gevonden, default Chrome browser!');
-    return chrome;
-  }
-};
+const activeBrowser =
+  browsers.find((browser) => browser.isActive) || browsers.find((browser) => browser.name === 'chrome');
 
-const browserVersion = () => {
-  switch (browserName()) {
-    case chrome:
-      return '85.0';
-    case firefox:
-      return '83.0';
-    case edge:
-      return '86.0';
-    default:
-      'latest';
-  }
-};
-
-const browserstack = () => process.argv.includes('browserstack') || yargs.browserstack;
-const sbRoot = yargs.local ? '' : 'storybook-static/';
-const basePort = yargs.local ? '8081' : '8080';
+const sbRoot = argv.local ? '' : 'storybook-static/';
+const basePort = argv.local ? '8081' : '8080';
 
 export const config = {
-  osName: osName(),
-  osVersion: osVersion(),
-  browserName: browserName(),
-  browserVersion: browserVersion(),
-  browserstack: browserstack(),
+  osName: activeBrowser.os.name,
+  osVersion: activeBrowser.os.version,
+  browserName: activeBrowser.name,
+  browserVersion: activeBrowser.version,
+  browserstack: argv.browserstack,
   gridUrl: 'http://selenium-hub:4444/wd/hub',
   baseUrl: `http://localhost:${basePort}/src/`,
   sbUrl: `http://localhost:8080/${sbRoot}iframe.html`,
