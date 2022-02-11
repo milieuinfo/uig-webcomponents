2cbc7edc1f54b3c675e24bce25256d54713f3ac7
Lucas Sledsens <lucas_sledsens@hotmail.com>
2022-02-11 10:48:17 +0100

    start modal

--- src/components/modal/modal.stories.js
+++ src/components/modal/modal.stories.js
@@ -1,8 +1,16 @@
 import { html } from 'lit-html';
+import { stylesheet } from '../../../.storybook/utils.js';
+import '../button';
+import buttonStyles from '../button/styles.scss';
 import './new';
 
 export default {
   title: 'custom-elements/vl-modal',
+  decorators: [(story) => html`${stylesheet(buttonStyles)}${story()}`],
+  args: { open: true },
 };
 
-export const Default = html`<new-modal></new-modal>`;
+export const Default = ({ open }) => html`<new-modal data-vl-title="Title test" .open=${open}>
+  <span slot="content">Content test!</span>
+  <button is="vl-button" slot="button">Start aanvraag</button>
+</new-modal>`;
--- src/components/modal/new.js
+++ src/components/modal/new.js
@@ -1,8 +1,11 @@
+/* eslint-disable lit-a11y/no-redundant-role */
+/* eslint-disable no-undef */
 import { html, css, LitElement, unsafeCSS } from 'lit';
+import { ref, createRef } from 'lit/directives/ref.js';
 import styles from '../modal/styles.scss';
-// import './lib';
+import '@govflanders/vl-ui-modal';
 
-export class SimpleGreeting extends LitElement {
+export class NewModal extends LitElement {
   static get styles() {
     return [
       css`
@@ -11,20 +14,62 @@ export class SimpleGreeting extends LitElement {
     ];
   }
 
+  static get properties() {
+    return {
+      open: { type: Boolean },
+      title: { type: String, attribute: 'data-vl-title', reflect: true },
+    };
+  }
+
+  constructor() {
+    super();
+    this.modalRef = createRef();
+  }
+
+  updated(changedProperties) {
+    changedProperties.forEach((oldValue, propName) => {
+      switch (propName) {
+        case 'open':
+          if (this.modalRef.value) {
+            vl.modal.toggle(this.modalRef.value);
+          }
+          break;
+        default:
+          break;
+      }
+    });
+  }
+
+  firstUpdated() {
+    vl.modal.dress(this.modalRef.value);
+  }
+
   render() {
     return html` <div class="vl-modal">
       <dialog
+        ${ref(this.modalRef)}
         class="vl-modal-dialog"
         data-vl-modal
+        data-vl-modal-closable
         tabindex="-1"
+        role="dialog"
         aria-modal="true"
         aria-hidden="true"
-        aria-labelledby="modal-toggle-title"
-        aria-describedby="modal-toggle-description"
+        aria-labelledby="modal-toggle-1-title"
+        aria-describedby="modal-toggle-1-description"
       >
-        <slot></slot>
+        <h2 class="vl-modal-dialog__title">${this.title}</h2>
+        <div class="vl-modal-dialog__content"><slot name="content">Modal content</slot></div>
+        <div class="vl-modal-dialog__footer">
+          <div class="vl-action-group">
+            <slot name="button" data-vl-modal-close></slot>
+            <!-- <a href="documentation/js-components/vl-ui-modal/#" class="vl-link" data-vl-modal-close>
+              <span class="vl-link__icon vl-link__icon--before vl-vi vl-vi-cross" aria-hidden="true"></span>Annuleer
+            </a> -->
+          </div>
+        </div>
       </dialog>
     </div>`;
   }
 }
-customElements.define('new-modal', SimpleGreeting);
+customElements.define('new-modal', NewModal);
