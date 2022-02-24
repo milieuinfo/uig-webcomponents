3f1fc2de6610b38cca0777ea1ea181251b4f530c
Maike Work <maike.meuris@cegeka.com>
2022-02-24 15:40:42 +0100

    Tests

---
+++ src/components/tabs/test/e2e/tab.js
@@ -0,0 +1,36 @@
+import { VlElement, By } from '../../../../utils/test';
+
+export default class VlTab extends VlElement {
+  async click() {
+    const nodes = await this.getTitleSlotNodes();
+    if (nodes && nodes.length > 0) {
+      return nodes[0].click();
+    }
+    const link = await this._getLink();
+    return link.click();
+  }
+
+  async getTitleSlotNodes() {
+    const slot = await this._getTitleSlot();
+    return this.getAssignedNodes(slot);
+  }
+
+  async isActive() {
+    return this.hasClass('vl-tab--active');
+  }
+
+  async getText() {
+    const slot = await this._getTitleSlot();
+    return slot.getText();
+  }
+
+  async _getLink() {
+    const element = await this.findElement(By.css('a'));
+    return new VlElement(this.driver, element);
+  }
+
+  async _getTitleSlot() {
+    const link = await this._getLink();
+    return link.findElement(By.css('slot'));
+  }
+}
