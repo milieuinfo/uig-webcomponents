d226bf25623975aaf26a413a1eec04a94816966c
Maike Work <maike.meuris@cegeka.com>
2022-02-25 09:11:08 +0100

    Migrate component

--- docs/pages/changelog/index.js
+++ docs/pages/changelog/index.js
@@ -5,7 +5,18 @@ import titleStyles from '../../../src/components/titles/styles.scss';
 import introStyles from '../../../src/components/introduction/styles.scss';
 import { removeStorybooksDefaultStyling } from '../../../.storybook/utils';
 
-const unreleased = [];
+const unreleased = [
+  {
+    version: 'Unreleased',
+    date: '?',
+    children: html`<ul>
+      <li>
+        <p><code>vl-video-player</code></p>
+        <p>The component has been migrated.</p>
+      </li>
+    </ul>`,
+  },
+];
 
 const changes = [
   ...unreleased,
