de27315c25d8abf3cea546f9437051fc22f69b00
Maike Work <maike.meuris@cegeka.com>
2022-03-07 16:19:05 +0100

    Add breakpoint logic + improvements

--- src/components/tabs/new.js
+++ src/components/tabs/new.js
@@ -14,51 +14,120 @@ export class VlTabsNew extends LitElement {
   constructor() {
     super();
     this.currentTabIndex = -1;
+    this.breakpointValue = null;
+    this.useHash = false;
+    this._responsiveNavigationOpen = false;
   }
 
-  firstUpdated() {
-    if (this.useHash) {
-      // Check if url has hash and open corresponding tab
-      const windowObject = window.location !== window.parent.location ? window.parent : window;
-      const hashTab = this.shadowRoot.querySelector(`[href='${windowObject.location.hash}']`);
-      if (hashTab) {
-        hashTab.click();
-      }
+  _debounce(func, timeout = 300) {
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        func.apply(this, args);
+      }, timeout);
+    };
+  }
 
-      // Open default active tab so hash shows in url
-      if (this.activeTab) {
-        const activeTabComponent = this.shadowRoot.querySelector(`[href='#${this.activeTab}']`);
-        if (activeTabComponent) {
-          activeTabComponent.click();
-        }
-      }
-    }
+  _getBreakpoint() {
+    // Transfer css breakpoints to JS
+    return window.getComputedStyle(document.body, ':before').getPropertyValue('content').replace(/"/g, '');
+  }
+
+  _getTabIndex(tabId) {
+    const children = [...this.children];
+    return children.findIndex((child) => child.tabId === tabId);
   }
 
   _activateTab(tabId, index) {
     if (this.currentTabIndex !== index) {
       this.activeTab = tabId;
-      this.currentTabIndex = index;
+
+      if (index) {
+        this.currentTabIndex = index;
+      } else {
+        this.currentTabIndex = this._getTabIndex(tabId);
+      }
     }
+  }
+
+  _deactivateTab() {
+    this.activeTab = undefined;
+    this.currentTabIndex = -1;
+  }
+
+  _handleResize() {
+    this.breakpointValue = this._getBreakpoint();
+  }
+
+  _handleHashChange() {
+    const windowObject = window.location !== window.parent.location ? window.parent : window;
+    const hash = windowObject.location.hash.replace('#', '');
+
+    if (hash !== this.activeTab) {
+      const child = this._getChild(hash);
+      if (child) {
+        this._activateTab(child.tabId);
+      } else {
+        this._deactivateTab();
+      }
+    }
+  }
+
+  firstUpdated() {
+    if (this.useHash) {
+      const windowObject = window.location !== window.parent.location ? window.parent : window;
+
+      // Check if url has hash on load and open corresponding tab
+      const hashTab = this.shadowRoot.querySelector(`[href='${windowObject.location.hash}']`);
+      if (hashTab) {
+        this._activateTab(hashTab.id);
+      }
+    }
+
+    this.breakpointValue = this._getBreakpoint();
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    window.addEventListener(
+      'resize',
+      this._debounce(() => {
+        this._handleResize();
+      }),
+      50,
+    );
+  }
+
+  disconnectedCallback() {
+    window.removeEventListener(
+      'resize',
+      this._debounce(() => {
+        this._handleResize();
+      }),
+      50,
+    );
+    window.removeEventListener('hashchange', () => this._handleHashChange(), false);
+
+    super.disconnectedCallback();
+  }
+
+  _getChild(tabId) {
+    const children = [...this.children];
+    return children.find((child) => child.tabId === tabId);
   }
 
   _handleClick(event, tabId, index) {
     event.preventDefault();
-
     this._activateTab(tabId, index);
-
-    //   // this.updateResponsiveBtnLabelForTabsContainerWithTab(tabsContainer, tab);
-
-    //   // const toggleBtnEl = tabsContainer.querySelector(`[${tabToggleAtt}]`);
-    //   // toggleBtnEl.click();
-    // }
+    this._responsiveNavigationOpen = !this._responsiveNavigationOpen;
   }
 
   _handleFocus(event, tabId, index) {
     if (!this.useHash) {
       event.preventDefault();
     }
-
     this._activateTab(tabId, index);
   }
 
@@ -109,41 +178,13 @@ export class VlTabsNew extends LitElement {
     }
   }
 
-  // updateResponsiveBtnLabelForTabsContainerWithTab(tabsContainer, tab) {
-  //   const toggleBtnEl = tabsContainer.querySelector('['.concat(tabToggleAtt, '] span'));
-  //   toggleBtnEl.innerHTML = tab.innerHTML;
-  // }
-
-  // setupResponsiveToggleBtnForTabsContainer(tabsContainer) {
-  //   const toggleBtnEl = tabsContainer.querySelector('['.concat(tabToggleAtt, ']'));
-
-  //   // const bp = breakpoint._getBreakpoint(); // setup responsive toggle btn
-
-  //   // if (bp === 'xsmall' || bp === 'small') {
-  //   //   toggleBtnEl.addEventListener('click', this.clickEvent, false);
-  //   // }
-  // }
-
-  // clickEvent(event) {
-  //   if (event.target && typeof event.target.closest === 'function') {
-  //     // check for IE
-  //     const tabsContainer = event.target.closest('['.concat(tabsAtt, ']'));
-  //     const toggleBtnEl = tabsContainer.querySelector('['.concat(tabToggleAtt, ']'));
-  //     const tabsList = tabsContainer.querySelector(''.concat(dataTabList));
-  //     const isListOpen = tabsList.getAttribute(tabShowAtt) === 'true';
-  //     tabsList.setAttribute(tabShowAtt, isListOpen ? 'false' : 'true');
-  //     tabsList.setAttribute('aria-hidden', isListOpen ? 'true' : 'false');
-  //     toggleBtnEl.setAttribute('aria-expanded', isListOpen ? 'true' : 'false');
-  //     toggleBtnEl.setAttribute(tabCloseAtt, isListOpen ? 'false' : 'true');
-  //   }
-  // }
-
   static get properties() {
     return {
       responsiveLabel: { type: String, attribute: 'data-vl-responsive-label', reflect: true },
       activeTab: { type: String, attribute: 'data-vl-active-tab', reflect: true },
       useHash: { type: Boolean, attribute: 'data-vl-use-hash', reflect: true },
       alt: { type: Boolean, attribute: 'data-vl-alt', reflect: true },
+      _responsiveNavigationOpen: { state: true },
     };
   }
 
@@ -155,12 +196,19 @@ export class VlTabsNew extends LitElement {
         case 'activeTab':
           this.dispatchEvent(new CustomEvent('change', { detail: { activeTab: this.activeTab }, composed: true }));
 
-          if (this.useHash) {
+          if (this.useHash && this.activeTab) {
             windowObject.location.hash = this.activeTab;
           }
 
           break;
 
+        case 'useHash':
+          if (this.useHash) {
+            windowObject.addEventListener('hashchange', () => this._handleHashChange(), false);
+          } else {
+            window.removeEventListener('hashchange', () => this._handleHashChange(), false);
+          }
+          break;
         default:
           break;
       }
@@ -170,33 +218,52 @@ export class VlTabsNew extends LitElement {
   render() {
     const children = [...this.children];
     const noDefaultActiveTab = !this.activeTab || this.activeTab === 'undefined';
+    const activeTabChild = this.activeTab && this._getChild(this.activeTab);
 
     return html`<div data-vl-tabs data-vl-tabs-responsive-label="${this.responsiveLabel}">
       <div class="vl-tabs__wrapper">
-        <ul class="vl-tabs ${this.alt ? 'vl-tabs--alt' : ''}" data-vl-tabs-list role="tablist">
-          ${children.map(
-            (child, index) =>
-              html`
-                <li class="vl-tab ${child.tabId === this.activeTab ? 'vl-tab--active' : ''}" data-vl-id=${child.tabId}>
-                  <a
-                    href=${`#${child.tabId}`}
-                    class="vl-tab__link"
-                    data-vl-tab
-                    role="tab"
-                    id=${child.tabId}
-                    tabindex=${(noDefaultActiveTab && index === 0) || child.tabId === this.activeTab ? '0' : '-1'}
-                    aria-selected=${child.tabId === this.activeTab}
-                    @click=${(e) => this._handleClick(e, child.tabId, index)}
-                    @focus=${(e) => this._handleFocus(e, child.tabId, index)}
-                    @keydown=${(e) => this._handleKeyDown(e)}
-                    >${child.title ? child.title : html`<slot name="title"></slot>`}</a
-                  >
-                </li>
-              `,
-          )}
+        <ul
+          class="vl-tabs ${this.alt ? 'vl-tabs--alt' : ''}"
+          data-vl-tabs-list
+          data-vl-show=${this._responsiveNavigationOpen}
+          aria-hidden=${!this._responsiveNavigationOpen}
+          role="tablist"
+        >
+          ${children.map((child, index) => {
+            const isActive = child.tabId === this.activeTab;
+
+            return html`
+              <li class="vl-tab ${isActive ? 'vl-tab--active' : ''}" data-vl-id=${child.tabId}>
+                <a
+                  href=${`#${child.tabId}`}
+                  class="vl-tab__link"
+                  data-vl-tab
+                  role="tab"
+                  id=${child.tabId}
+                  tabindex=${(noDefaultActiveTab && index === 0) || isActive ? '0' : '-1'}
+                  aria-selected=${isActive}
+                  @click=${(e) => this._handleClick(e, child.tabId, index)}
+                  @focus=${(e) => this._handleFocus(e, child.tabId, index)}
+                  @keydown=${(e) => this._handleKeyDown(e)}
+                  >${child.title ? child.title : html`<slot name="title"></slot>`}</a
+                >
+              </li>
+            `;
+          })}
         </ul>
-        <button type="button" data-vl-tabs-toggle aria-expanded="false" class="vl-tabs__toggle" data-vl-close="false">
-          <span>${this.responsiveLabel || 'Navigatie'}</span>
+        <button
+          type="button"
+          data-vl-tabs-toggle
+          aria-expanded=${!this._responsiveNavigationOpen}
+          class="vl-tabs__toggle"
+          data-vl-close=${this._responsiveNavigationOpen}
+          @click=${() => {
+            if (this.breakpointValue === 'xsmall' || this.breakpointValue === 'small') {
+              this._responsiveNavigationOpen = !this._responsiveNavigationOpen;
+            }
+          }}
+        >
+          <span>${activeTabChild ? activeTabChild.title : this.responsiveLabel || 'Navigatie'}</span>
         </button>
       </div>
 
