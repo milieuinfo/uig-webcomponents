ed3b313c2e737e9be89ccb057b8fa03d4d0810c0
Maike Work <maike.meuris@cegeka.com>
2022-03-08 11:27:59 +0100

    Component migration as is

---
+++ src/components/info-tile/index.js
@@ -0,0 +1,143 @@
+import { vlElement, define } from '../../utils/core';
+import '../accordion';
+import styles from './styles.scss';
+
+export class VlInfoTile extends vlElement(HTMLElement) {
+  static get _observedAttributes() {
+    return ['auto-open', 'toggleable'];
+  }
+
+  constructor() {
+    super(`
+      <style>
+        ${styles}
+      </style>
+      <div class="vl-info-tile">
+        <header class="vl-info-tile__header" role="presentation">
+          <div id="wrapper" class="vl-info-tile__header__wrapper">
+            <h3 id="title" class="vl-info-tile__header__title">
+              <slot name="title"></slot><slot name="title-label"></slot>
+            </h3>
+            <p class="vl-info-tile__header__subtitle">
+              <slot name="subtitle"></slot>
+            </p>
+          </div>
+        </header>
+
+        <div class="vl-info-tile__content">
+          <slot name="content"></slot>
+        </div>
+
+        <footer class="vl-info-tile__footer">
+          <slot name="footer"></slot>
+        </footer>
+      </div>
+    `);
+  }
+
+  connectedCallback() {
+    this.__processAutoOpen();
+    this.__processSlots();
+  }
+
+  get isToggleable() {
+    return this.getAttribute('toggleable') !== null;
+  }
+
+  get isOpen() {
+    if (this.isToggleable) {
+      return this._element.classList.contains('js-vl-accordion--open');
+    }
+    return true;
+  }
+
+  get _headerWrapperElement() {
+    return this._element.querySelector('#wrapper');
+  }
+
+  get _titleElement() {
+    return this._headerWrapperElement.querySelector('#title');
+  }
+
+  get _titleLabelSlot() {
+    return this.querySelector("[slot='title-label']");
+  }
+
+  get _titleLabelSlotElement() {
+    return this._titleElement.querySelector('[name="title-label"]');
+  }
+
+  get _buttonElement() {
+    return this._element.querySelector('button');
+  }
+
+  toggle() {
+    this._toggleElement.click();
+  }
+
+  open() {
+    if (!this.isOpen) {
+      this.toggle();
+    }
+  }
+
+  close() {
+    if (this.isOpen) {
+      this.toggle();
+    }
+  }
+
+  get _toggleElement() {
+    return this._shadow.querySelector('.js-vl-accordion__toggle');
+  }
+
+  get _subtitleElement() {
+    return this._shadow.querySelector('slot[name="subtitle"]');
+  }
+
+  get _contentElement() {
+    return this._shadow.querySelector('slot[name="content"]');
+  }
+
+  _toggleableChangedCallback(oldValue, newValue) {
+    if (newValue !== null) {
+      this.__prepareAccordionElements();
+      vl.accordion.dress(this._buttonElement);
+      this.__preventContentClickPropagation();
+    }
+  }
+
+  __prepareAccordionElements() {
+    this._element.classList.add('js-vl-accordion');
+    const button = this._template(`
+      <button class="vl-toggle vl-link vl-link--bold js-vl-accordion__toggle">
+        <i class="vl-link__icon vl-link__icon--before vl-toggle__icon vl-vi vl-vi-arrow-right-fat" aria-hidden="true"></i>
+      </button>
+    `).firstElementChild;
+    button.appendChild(this._titleElement);
+    this._headerWrapperElement.prepend(button);
+  }
+
+  __preventContentClickPropagation() {
+    this._subtitleElement.addEventListener('click', (e) => e.stopPropagation());
+    this._contentElement.addEventListener('click', (e) => e.stopPropagation());
+  }
+
+  __processAutoOpen() {
+    if (this.isToggleable && this.dataset.vlAutoOpen !== null) {
+      this.open();
+    }
+  }
+
+  __processSlots() {
+    if (!this._titleLabelSlot) {
+      this._titleLabelSlotElement.remove();
+    }
+    this._titleElement.addEventListener('click', (event) => {
+      event.stopPropagation();
+      this._buttonElement.click();
+    });
+  }
+}
+
+define('vl-info-tile', VlInfoTile);
---
+++ src/components/info-tile/test/e2e/info-tile.spec.js
@@ -0,0 +1,63 @@
+import { assert, getDriver, config } from '../../../../utils/test';
+import { VlInfoTile } from './info-tile';
+
+const { sbUrl } = config;
+const defaultUrl = `${sbUrl}?id=native-elements-vl-info-tile--default`;
+const toggleableUrl = `${sbUrl}?id=native-elements-vl-info-tile--default&args=toggleable:true`;
+const toggleableAutoOpenUrl = `${sbUrl}?id=native-elements-vl-info-tile--default&args=autoOpen:true;toggleable:true`;
+const selector = 'vl-info-tile';
+
+describe('vl-info-tile', async () => {
+  let driver;
+
+  beforeEach(() => {
+    driver = getDriver();
+    driver.manage().window().maximize();
+  });
+
+  it('as a user I can see the tilte of the info tile', async () => {
+    await driver.get(defaultUrl);
+    const infoTile = await new VlInfoTile(driver, selector);
+    await assert.eventually.equal(infoTile.getTitle(), 'Broos Deprez');
+  });
+
+  it('as a user I can see the subtitle of the info tile', async () => {
+    await driver.get(defaultUrl);
+    const infoTile = await new VlInfoTile(driver, selector);
+    await assert.eventually.equal(infoTile.getSubtitle(), 'Uw zoon (19.05.2005)');
+  });
+
+  it('as a user I can see the content of the info tile', async () => {
+    await driver.get(defaultUrl);
+    const infoTile = await new VlInfoTile(driver, selector);
+    const contentSlotElement = await infoTile.getContentSlotElement();
+    await assert.eventually.include(contentSlotElement.getText(), 'De studietoelage voor Broos Deprez werd toegekend.');
+  });
+
+  it('as a user I can open and close a toggleable info tile', async () => {
+    await driver.get(toggleableUrl);
+    const infoTile = await new VlInfoTile(driver, selector);
+    await assert.eventually.isFalse(infoTile.isOpen());
+    await infoTile.toggle();
+    await assert.eventually.isTrue(infoTile.isOpen());
+    await infoTile.toggle();
+    await assert.eventually.isFalse(infoTile.isOpen());
+  });
+
+  it('as a user I can open and close a toggleable info tile by clicking on the title', async () => {
+    await driver.get(toggleableUrl);
+    const infoTile = await new VlInfoTile(driver, selector);
+    await assert.eventually.isFalse(infoTile.isOpen());
+    const slots = await infoTile.getTitleSlotElements();
+    await slots[0].click();
+    await assert.eventually.isTrue(infoTile.isOpen());
+    await slots[0].click();
+    await assert.eventually.isFalse(infoTile.isOpen());
+  });
+
+  it("as a user I can immediately see the content of an info tile when it's automatically open", async () => {
+    await driver.get(toggleableAutoOpenUrl);
+    const infoTile = await new VlInfoTile(driver, selector);
+    await assert.eventually.isTrue(infoTile.isOpen());
+  });
+});
