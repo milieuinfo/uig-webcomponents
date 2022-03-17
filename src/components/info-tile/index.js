cc9fb74c15208fb7a88fda3faa876d17154985fa
Maike Work <maike.meuris@cegeka.com>
2022-03-17 16:10:29 +0100

    Make auto open only set open/closed on load & toggleable

--- src/components/info-tile/index.js
+++ src/components/info-tile/index.js
@@ -112,10 +112,6 @@ export class VlInfoTile extends vlElement(HTMLElement) {
     }
   }
 
-  _autoOpenChangedCallback() {
-    this.__processAutoOpen();
-  }
-
   __prepareAccordionElements() {
     this._element.classList.add('js-vl-accordion');
     const button = this._template(`
