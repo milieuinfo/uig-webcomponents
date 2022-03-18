70b50102bad99545b47b3f488cc9f7b790956f27
Maike Work <maike.meuris@cegeka.com>
2022-03-18 15:25:17 +0100

    Fix PR comments

--- src/legacy/upload/upload.stories.js
+++ src/legacy/upload/upload.stories.js
@@ -2,31 +2,31 @@ import { html } from 'lit-html';
 import '../upload';
 import styles from '../../components/button/styles.scss';
 import { stylesheet, docsIntro } from '../../../.storybook/utils.js';
+import linkStyles from '../../components/link';
 
 export default {
-  title: 'legacy-elements/vl-upload',
-  decorators: [(story) => html`${stylesheet(styles)}${story()}`],
+  title: 'legacy/vl-upload',
+  decorators: [(story) => html`${stylesheet(`${styles}${linkStyles}`)}${story()}`],
   parameters: {
     controls: { hideNoControlsWarning: true },
     docs: {
       description: {
-        component: docsIntro(
-          {
-            root: 'upload',
-            intro:
-              'Use the upload component to select or drag and drop one or more files in the upload field. Alternatively, the user can upload one or more files by clicking the link in the upload field and selecting the file(s) in the File menu.',
-          },
-          true,
-        ),
+        component: docsIntro({
+          root: 'upload',
+          intro:
+            'Use the upload component to select or drag and drop one or more files in the upload field. Alternatively, the user can upload one or more files by clicking the link in the upload field and selecting the file(s) in the File menu.',
+          isLegacy: true,
+        }),
       },
     },
   },
 };
 
-export const Default = () =>
-  html`<p>
-    Documentatie:
-    <a href="https://webcomponenten.omgeving.vlaanderen.be/doc/VlUpload.html"
-      >https://webcomponenten.omgeving.vlaanderen.be/doc/VlUpload.html</a
-    >
-  </p>`;
+export const Default = () => html`<a
+  is="vl-link"
+  target="_blank"
+  data-vl-inline
+  href="https://webcomponenten.omgeving.vlaanderen.be/doc/VlUpload.html"
+>
+  Legacy docs <span is="vl-icon" data-vl-icon="external" data-vl-after data-vl-link></span
+></a>`;
