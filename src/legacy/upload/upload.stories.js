bdf8c9e3279e4bf41b2a796b390f5d0818f353ca
Maike Work <maike.meuris@cegeka.com>
2022-03-21 13:36:14 +0100

    PR changes

--- src/legacy/upload/upload.stories.js
+++ src/legacy/upload/upload.stories.js
@@ -1,20 +1,15 @@
 import { html } from 'lit-html';
 import '../upload';
-import styles from '../../components/button/styles.scss';
-import { stylesheet, docsIntro } from '../../../.storybook/utils.js';
-import linkStyles from '../../components/link/styles.scss';
+import { docsIntro } from '../../../.storybook/utils.js';
 
 export default {
   title: 'legacy/vl-upload',
-  decorators: [(story) => html`${stylesheet(`${styles}${linkStyles}`)}${story()}`],
   parameters: {
     controls: { hideNoControlsWarning: true },
     docs: {
       description: {
         component: docsIntro({
           root: 'upload',
-          intro:
-            'Use the upload component to select or drag and drop one or more files in the upload field. Alternatively, the user can upload one or more files by clicking the link in the upload field and selecting the file(s) in the File menu.',
           isLegacy: true,
         }),
       },
