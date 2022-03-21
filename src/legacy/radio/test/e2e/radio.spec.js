7316b1db4f2ca515340caa0200a58cf92e06e064
Maike Work <maike.meuris@cegeka.com>
2022-03-21 13:55:08 +0100

    Translate tests

--- src/legacy/radio/test/e2e/radio.spec.js
+++ src/legacy/radio/test/e2e/radio.spec.js
@@ -11,7 +11,7 @@ describe('vl-radio', async () => {
     return vlRadioPage.load();
   });
 
-  it('als gebruiker kan ik het radio label zien', async () => {
+  it('as a user I can see the radio label', async () => {
     const radio = await vlRadioPage.getCheckedRadio(1);
     const slotLabelRadio = await vlRadioPage.getSlotLabelRadio(1);
     const slotLabels = await slotLabelRadio.labelSlotElements();
@@ -19,7 +19,7 @@ describe('vl-radio', async () => {
     await assert.eventually.equal(slotLabels[0].getText(), 'Ja');
   });
 
-  it('als gebruiker kan ik maar 1 radio tegelijkertijd aanvinken', async () => {
+  it('as a user I can only check one radio at a time', async () => {
     const radio1 = await vlRadioPage.getRadio(1);
     const radio2 = await vlRadioPage.getRadio(2);
 
@@ -33,21 +33,21 @@ describe('vl-radio', async () => {
     await assert.eventually.isTrue(radio2.isChecked());
   });
 
-  it('als gebruiker kan ik het verschil zien tussen een block en een gewone radio', async () => {
+  it('as a user I can see the difference between a block radio and a normal radio', async () => {
     const radio = await vlRadioPage.getRadio(1);
     const blockRadio = await vlRadioPage.getBlockRadio(1);
     await assert.eventually.isFalse(radio.isBlock());
     await assert.eventually.isTrue(blockRadio.isBlock());
   });
 
-  it('als gebruiker kan ik het verschil zien tussen een error en een gewone radio', async () => {
+  it('as a user I can see the difference between an error radio and a normal radio', async () => {
     const radio = await vlRadioPage.getRadio(1);
     const errorRadio = await vlRadioPage.getErrorRadio(1);
     await assert.eventually.isFalse(radio.hasError());
     await assert.eventually.isTrue(errorRadio.hasError());
   });
 
-  it('als gebruiker kan ik een disabled radio niet selecteren', async () => {
+  it(`as a user I can't select a disabled radio`, async () => {
     const radio = await vlRadioPage.getRadio(1);
     const disabledRadio = await vlRadioPage.getDisabledRadio(1);
     await assert.eventually.isFalse(radio.isDisabled());
@@ -60,21 +60,21 @@ describe('vl-radio', async () => {
     await assert.eventually.isTrue(disabledRadio.isDisabled());
   });
 
-  it('als gebruiker kan ik het verschil zien tussen een single en een gewone radio', async () => {
+  it('as a user I can see the difference between a single radio and a normal radio', async () => {
     const radio = await vlRadioPage.getRadio(1);
     const singleRadio = await vlRadioPage.getSingleRadio(1);
     await assert.eventually.isFalse(radio.isSingle());
     await assert.eventually.isTrue(singleRadio.isSingle());
   });
 
-  it('als gebruiker kan ik zien dat een radio standaard aangevinkt staat', async () => {
+  it('as a user I can see a radio is default checked', async () => {
     const radio1 = await vlRadioPage.getCheckedRadio(1);
     const radio2 = await vlRadioPage.getCheckedRadio(2);
     await assert.eventually.isFalse(radio1.isChecked());
     await assert.eventually.isTrue(radio2.isChecked());
   });
 
-  it('als gebruiker kan ik een radio aanvinken door gebruik te maken van de pijl toetsen', async () => {
+  it('as a user I can check a radio user the arrow keys', async () => {
     if (config.browserName === 'chrome') {
       const html = await driver.findElement(By.css('html'));
       const radio1 = await vlRadioPage.getRadio(1);
@@ -109,7 +109,7 @@ describe('vl-radio', async () => {
     }
   });
 
-  it('als gebruiker kan ik door gebruik te maken van de pijl toetsen van de laatste radio naar de eerste gaan en omgekeerd', async () => {
+  it('as a user I can go from the last radio to the first one and back', async () => {
     if (config.browserName === 'chrome') {
       const html = await driver.findElement(By.css('html'));
       const radio1 = await vlRadioPage.getRadio(1);
@@ -144,7 +144,7 @@ describe('vl-radio', async () => {
     }
   });
 
-  it('als gebruiker zie ik dat het reeds geselecteerde radio element focus krijgt bij het tabben', async () => {
+  it('as a user I can see the already checked radio gets focus while tabbing', async () => {
     if (config.browserName === 'chrome') {
       const html = await driver.findElement(By.css('html'));
       const radio1 = await vlRadioPage.getRadio(1);
@@ -168,7 +168,7 @@ describe('vl-radio', async () => {
     }
   });
 
-  // it('als gebruiker kan ik een radio met een shadow DOM parent aanvinken door gebruik te maken van de pijl toetsen', async () => {
+  // it('as a user I can check a radio with a shadow DOM using the arrow keys', async () => {
   //   const radios = await vlRadioPage.getShadowDOMRadioElement();
   //   const radio1 = await vlRadioPage.getShadowDOMRadio(1);
   //   const radio2 = await vlRadioPage.getShadowDOMRadio(2);
