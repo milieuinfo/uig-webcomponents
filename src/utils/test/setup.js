1f0a43716badb47fff1ad885c3787ffd67ec11e7
Lucas Sledsens <lucas_sledsens@hotmail.com>
2022-03-28 12:03:51 +0200

    Update setup.js

--- src/utils/test/setup.js
+++ src/utils/test/setup.js
@@ -21,7 +21,7 @@ const capabilities = {
   'browserstack.key': process.env.browserstack_password,
   'browserstack.local': true,
   'browserstack.localIdentifier': identifier,
-  'browserstack.selenium_version': '4.1.1',
+  'browserstack.selenium_version': '4.1.2',
   'browserstack.idleTimeout': 300,
   'browserstack.SO_TIMEOUT': 300,
   'browserstack.networkLogs': true,
