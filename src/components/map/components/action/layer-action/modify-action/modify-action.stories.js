464f28878ad97b93c4386f9a8b7d5cf2f4df235c
Sander Kleykens <sander@kleykens.com>
2022-03-29 12:28:12 +0200

    Introduceer de mogelijkheid om bij gebruik van de modify action te snappen op de features van één of meerdere layers (#92)
    * Voeg de mogelijkheid toe om te snappen op kaartlagen tijdens de modify action
    
    Co-authored-by: Lucas Sledsens <lucas_sledsens@hotmail.com>

--- src/components/map/components/action/layer-action/modify-action/modify-action.stories.js
+++ src/components/map/components/action/layer-action/modify-action/modify-action.stories.js
@@ -1,38 +1,57 @@
-import { html } from "lit-html";
-import "../../../../../map";
-import { args, argTypes } from "../config";
-import { docsIntro } from "../../../../../../../.storybook/utils.js";
+import { html } from 'lit-html';
+import '../../../../../map';
+import { args, argTypes } from '../config';
+import { docsIntro } from '../../../../../../../.storybook/utils.js';
 
 export default {
-  title: "custom-elements/vl-map/vl-map-modify-action",
+  title: 'custom-elements/vl-map/vl-map-modify-action',
   parameters: {
     controls: { hideNoControlsWarning: true },
     docs: {
       description: {
         component: docsIntro({
-          root: "map",
-          intro: "De kaart aanpas actie component.",
+          root: 'map',
+          intro: 'De kaart aanpas actie component.',
         }),
       },
     },
   },
-  args,
-  argTypes,
+  args: { ...args, snapping: true, snappingPixelTolerance: 1000 },
+  argTypes: {
+    ...argTypes,
+    snapping: {
+      name: 'data-vl-snapping',
+      type: { summary: 'boolean' },
+      description: 'Attribute enables snapping on the vl-map-wfs-layers that are added to this action.',
+      table: {
+        defaultValue: { summary: 'false' },
+      },
+    },
+    snappingPixelTolerance: {
+      name: 'data-vl-snapping-pixel-tolerance',
+      type: { summary: 'number' },
+      description:
+        'Attribute configures the maximum distance (in pixels) between a feature and your pointing device before snapping occurs.',
+      table: {
+        defaultValue: { summary: '10' },
+      },
+    },
+  },
 };
 
 export const MapWithPointModify = () => {
   const features = {
-    type: "FeatureCollection",
+    type: 'FeatureCollection',
     features: [
       {
-        type: "Feature",
+        type: 'Feature',
         id: 1,
-        geometry: { type: "Point", coordinates: [157836.54, 190879.51] },
+        geometry: { type: 'Point', coordinates: [157836.54, 190879.51] },
       },
       {
-        type: "Feature",
+        type: 'Feature',
         id: 2,
-        geometry: { type: "Point", coordinates: [152161.53, 212358.26] },
+        geometry: { type: 'Point', coordinates: [152161.53, 212358.26] },
       },
     ],
   };
@@ -50,13 +69,13 @@ export const MapWithPointModify = () => {
 
 export const MapWithLineModify = () => {
   const features = {
-    type: "FeatureCollection",
+    type: 'FeatureCollection',
     features: [
       {
-        type: "Feature",
+        type: 'Feature',
         id: 1,
         geometry: {
-          type: "LineString",
+          type: 'LineString',
           coordinates: [
             [157836.54, 190879.51],
             [152161.53, 212358.26],
@@ -79,13 +98,13 @@ export const MapWithLineModify = () => {
 
 export const MapWithPolygonModify = () => {
   const features = {
-    type: "FeatureCollection",
+    type: 'FeatureCollection',
     features: [
       {
-        type: "Feature",
+        type: 'Feature',
         id: 1,
         geometry: {
-          type: "Polygon",
+          type: 'Polygon',
           coordinates: [
             [
               [104896.56, 193972.22],
@@ -105,6 +124,38 @@ export const MapWithPolygonModify = () => {
       <vl-map-features-layer data-vl-features=${JSON.stringify(features)}>
         <vl-map-layer-style></vl-map-layer-style>
         <vl-map-modify-action data-vl-default-active></vl-map-modify-action>
+      </vl-map-features-layer>
+    </vl-map>
+  `;
+};
+
+export const MapWithSnappingModify = ({ snapping, snappingPixelTolerance }) => {
+  const features = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        id: 1,
+        geometry: { type: 'Point', coordinates: [151285.5138477709, 211586.43498009123] },
+      },
+    ],
+  };
+
+  return html`
+    <vl-map id="map">
+      <vl-map-baselayer-grb-gray></vl-map-baselayer-grb-gray>
+      <vl-map-features-layer data-vl-features=${JSON.stringify(features)} data-vl-auto-extent>
+        <vl-map-layer-circle-style></vl-map-layer-circle-style>
+        <vl-map-modify-action 
+            data-vl-default-active
+            ?data-vl-snapping=${snapping}
+            data-vl-snapping-pixel-tolerance=${snappingPixelTolerance}>
+          <vl-map-wfs-layer
+              data-vl-name="Stromend waterlichamen"
+              data-vl-url="https://geoserver.vmm.be/geoserver/vmm/wfs"
+              data-vl-layers="owl_l"
+              data-vl-max-resolution="4">
+        </vl-map-modify-action>
       </vl-map-features-layer>
     </vl-map>
   `;
