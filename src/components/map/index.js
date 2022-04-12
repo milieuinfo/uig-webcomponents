8fa6f1d329542568b414a708732a09312434a799
maikemeu <99736244+maikemeu@users.noreply.github.com>
2022-04-12 18:00:03 +0200

    Add imports to index

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,3 +1,5 @@
+// Map component
+
 export { VlMap } from './components/map';
 export { VlMapOverviewMap } from './components/overview-map';
 export { VlMapLayer } from './components/layer';
@@ -28,3 +30,29 @@ export { VlMapSideSheetMenu } from './components/side-sheet-menu';
 export { VlMapSideSheetMenuItem } from './components/side-sheet-menu-item';
 export { VlMapModifyAction } from './components/action/layer-action/modify-action';
 export { VlSelectLocation } from './components/select-location';
+
+// Map actions (Layer between map component and OpenLayers)
+
+export { VlCustomMap } from './actions/custom-map';
+export { VlMapWithActions } from './actions/map-with-actions';
+export { VlBoxSelectAction } from './actions/box-select-action';
+export { VlDeleteAction } from './actions/delete-action';
+export { VlDrawAction } from './actions/draw-action';
+export { VlDrawLineAction } from './actions/draw-line-action';
+export { VlDrawPolygonAction } from './actions/draw-polygon-action';
+export { VlDrawRectangleAction } from './actions/draw-rectangle-action';
+export { VlHighlightAction } from './actions/highlight-action';
+export { VlMapAction as VlMapActionAction } from './actions/mapaction';
+export { VlMeasureAction } from './actions/measure-action';
+export { VlModifyAction } from './actions/modify-action';
+export { VlModifyAndTranslateAction } from './actions/modify-and-translate-action';
+export { VlSelectAction } from './actions/select-action';
+export { VlSelectActions } from './actions/select-actions';
+export { VlShowInfoAction } from './actions/show-info-action';
+export { VlShowInfoSelectAction } from './actions/show-info-select-action';
+export { VlSplitAction } from './actions/split-action';
+export { VlTooltips } from './actions/tooltips';
+export { VlTranslateAction } from './actions/translate-action';
+export { VlSnapInteraction } from './actions/snap-interaction';
+export { VlCompositeVectorLayer } from './actions/composite-vector-layer';
+export { VlCompositeVectorSource } from './actions/composite-vector-source';
