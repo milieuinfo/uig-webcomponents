016c557d5412385036715d0b301332d866da0a88
maikemeu <99736244+maikemeu@users.noreply.github.com>
2022-06-07 11:32:43 +0200

    Checkbox legacy

--- src/legacy/checkbox/checkbox.stories.js
+++ src/legacy/checkbox/checkbox.stories.js
@@ -1,6 +1,33 @@
-// import { html } from "lit-html";
-// import "../checkbox";
-// import styles from "./styles.scss";
+import { html } from "lit-html";
+import "../checkbox";
+import linkStyles from '../../components/link/styles.scss';
+import { docsIntro, stylesheet } from '../../../.storybook/utils.js';
+
+
+export default {
+  title: 'legacy/vl-checkbox',
+  decorators: [(story) => html`${stylesheet(linkStyles)}${story()}`],
+  parameters: {
+    controls: { hideNoControlsWarning: true },
+    docs: {
+      description: {
+        component: docsIntro({
+          root: 'checkbox',
+          isLegacy: true,
+        }),
+      },
+    },
+  },
+};
+
+export const Default = () => html`<a
+  is="vl-link"
+  target="_blank"
+  data-vl-inline
+  href="https://webcomponenten.omgeving.vlaanderen.be/doc/VlCheckbox.html"
+>
+  Legacy docs <span is="vl-icon" data-vl-icon="external" data-vl-after data-vl-link></span
+></a>`;
 
 // const defaultArgs = {
 //   block: false,
