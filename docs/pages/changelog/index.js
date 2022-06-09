35038dbba3e517b59683dff2f43ca35e5361c49d
maikemeu <99736244+maikemeu@users.noreply.github.com>
2022-06-09 09:49:02 +0200

    Legacy

--- docs/pages/changelog/index.js
+++ docs/pages/changelog/index.js
@@ -5,8 +5,18 @@ import titleStyles from '../../../src/components/titles/styles.scss';
 import introStyles from '../../../src/components/introduction/styles.scss';
 import { removeStorybooksDefaultStyling } from '../../../.storybook/utils';
 
-const unreleased = [];
-
+const unreleased = [
+  {
+    version: 'Unreleased',
+    date: '?',
+    children: html`<ul>
+      <li>
+        <p><code>vl-infotext</code></p>
+        <p>Added as a legacy component.</p>
+      </li>
+    </ul>`,
+  },
+];
 const changes = [
   ...unreleased,
   {
