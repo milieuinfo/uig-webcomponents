c581f0413d078b6d882d429dec50f5a6cf282a23
maikemeu <99736244+maikemeu@users.noreply.github.com>
2022-06-16 15:33:38 +0200

    Remove env variables

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,5 +1,4 @@
 import path from 'path';
-import Dotenv from 'dotenv-webpack';
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
@@ -10,5 +9,4 @@ export default {
     path: path.resolve(__dirname, '../build'),
     filename: 'index.js',
   },
-  plugins: [new Dotenv()],
 };
