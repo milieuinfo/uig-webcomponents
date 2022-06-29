493941c079ca8f747e1dd145b5004e5d2e7a84ce
Gert-Jan Meire <gertjan.meire@gmail.com>
2022-06-29 13:26:30 +0200

    Added fix for build script.

--- scripts/build.js
+++ scripts/build.js
@@ -33,7 +33,7 @@ const buildConfig = {
     'toaster',
     'video-player',
   ],
-  componentsWithStylesheetAndInlineStyling: ['button', 'pill', 'tooltip', 'textarea', 'typography', 'steps'],
+  componentsWithStylesheetAndInlineStyling: ['button', 'pill', 'tooltip', 'textarea', 'typography'],
 };
 
 const { src, dist, componentsWithStylesheet, componentsWithStylesheetAndInlineStyling } = buildConfig;
