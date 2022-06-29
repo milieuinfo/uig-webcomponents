9884afd21ef149f2854d485731d8cfff215d2f82
Gert-Jan Meire <gertjan.meire@gmail.com>
2022-06-29 12:14:28 +0200

    Added e2e tests.

--- scripts/build.js
+++ scripts/build.js
@@ -33,7 +33,7 @@ const buildConfig = {
     'toaster',
     'video-player',
   ],
-  componentsWithStylesheetAndInlineStyling: ['button', 'pill', 'tooltip', 'textarea', 'typography'],
+  componentsWithStylesheetAndInlineStyling: ['button', 'pill', 'tooltip', 'textarea', 'typography', 'steps'],
 };
 
 const { src, dist, componentsWithStylesheet, componentsWithStylesheetAndInlineStyling } = buildConfig;
