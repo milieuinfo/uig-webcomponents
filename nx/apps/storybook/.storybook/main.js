d913b22af84f5a3790e1fd8b2d897795462e3b85
Kris Speltincx <kris@bladerbeeld.be>
2022-07-12 21:08:55 +0200

    configuratie experiment net cleane 'npm install' gedaan

--- nx/apps/storybook/.storybook/main.js
+++ nx/apps/storybook/.storybook/main.js
@@ -1,5 +1,7 @@
+const path = require('path');
+
 module.exports = {
-  core: {
+  "core": {
     builder: 'webpack5',
   },
   "stories": [
@@ -10,5 +12,20 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
-  "framework": "@storybook/web-components"
+  "framework": "@storybook/web-components",
+  "webpackFinal": async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    // Return the altered config
+    return config;
+  },
 }
