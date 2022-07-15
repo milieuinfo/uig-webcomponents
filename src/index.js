7297c0cab50e79fb610a84f7930efd1bc13cc237
Gert-Jan Meire <gertjan.meire@gmail.com>
2022-07-15 11:14:28 +0200

    Added rich data table and unit testing.

--- src/index.js
+++ src/index.js
@@ -103,6 +103,7 @@ export { VlToaster } from './legacy/toaster';
 export { VlCheckbox } from './legacy/checkbox';
 export { VlInfotext } from './legacy/infotext';
 export { VlRichData } from './legacy/rich-data';
+export { VlRichDataTable } from './legacy/rich-data-table';
 export { VlProzaMessage } from './legacy/proza-message/src';
 export { VlProzaMessagePreloader } from './legacy/proza-message/src';
 export { VlMultiSelect } from './legacy/multiselect';
