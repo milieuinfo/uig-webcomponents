5dfae185499ac1b72dac3c4e64873ea5626c6c78
Gert-Jan Meire <gertjan.meire@gmail.com>
2022-07-18 15:45:46 +0200

    Fixed table styling issue.

--- src/legacy/rich-data-table/rich-data-table.stories.js
+++ src/legacy/rich-data-table/rich-data-table.stories.js
@@ -41,38 +41,38 @@ export const Test = () => {
   const data =
     '{"data": [{ "id" : 0, "name" : "Project #1" , "owner" : "Jan Jansens" }, { "id" : 1, "name" : "Project #2" , "owner" : "Jan Jansens" }]}';
 
-  // const tableSorter = (table) => {
-  //   const originalTableData = [...table.data.data];
-  //   return (event) => {
-  //     const {sorting} = event.detail;
-  //     const table = event.target;
-  //     if (sorting) {
-  //       table.data = {
-  //         data: [...originalTableData].sort((firstEl, secondEl) => {
-  //           for (let i = 0; i < sorting.length; i++) {
-  //             const criteria = sorting[i];
-  //             const firstValue = firstEl[criteria.name];
-  //             const secondValue = secondEl[criteria.name];
-  //             const isAscending = criteria.direction === 'asc';
-  //             if (firstValue < secondValue) {
-  //               return isAscending ? -1 : 1;
-  //             } else if (firstValue > secondValue) {
-  //               return isAscending ? 1 : -1;
-  //             }
-  //           }
-  //           return 0;
-  //         }),
-  //         sorting ,
-  //       };
-  //     } else {
-  //       table.data = originalTableData;
-  //     }
-  //   };
-  // };
-  // customElements.whenDefined('vl-rich-data-table').then(() => {
-  //   const table = document.querySelector('#rich-data-table-sorting');
-  //   table.addEventListener('change', tableSorter(table));
-  // });
+  const tableSorter = (table) => {
+    const originalTableData = [...table.data.data];
+    return (event) => {
+      const {sorting} = event.detail;
+      const table = event.target;
+      if (sorting) {
+        table.data = {
+          data: [...originalTableData].sort((firstEl, secondEl) => {
+            for (let i = 0; i < sorting.length; i++) {
+              const criteria = sorting[i];
+              const firstValue = firstEl[criteria.name];
+              const secondValue = secondEl[criteria.name];
+              const isAscending = criteria.direction === 'asc';
+              if (firstValue < secondValue) {
+                return isAscending ? -1 : 1;
+              } else if (firstValue > secondValue) {
+                return isAscending ? 1 : -1;
+              }
+            }
+            return 0;
+          }),
+          sorting ,
+        };
+      } else {
+        table.data = originalTableData;
+      }
+    };
+  };
+  customElements.whenDefined('vl-rich-data-table').then(() => {
+    const table = document.querySelector('#rich-data-table-sorting');
+    table.addEventListener('change', tableSorter(table));
+  });
 
   return html`
     <vl-rich-data-table id="rich-data-table-sorting" data-vl-data="${data}">
