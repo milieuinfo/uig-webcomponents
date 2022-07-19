00e291fe80b51357e1278b5093b3148eed09afac
Gert-Jan Meire <gertjan.meire@gmail.com>
2022-07-19 13:18:07 +0200

    Fixed close button icon for multiselect.

--- src/legacy/multiselect/multiselect.stories.js
+++ src/legacy/multiselect/multiselect.stories.js
@@ -1,6 +1,7 @@
 
 import { html } from 'lit-html';
 import '../../components/link';
+import '../../legacy/multiselect';
 import linkStyles from '../../components/link/styles.scss';
 import { docsIntro, stylesheet } from '../../../.storybook/utils.js';
 
@@ -28,3 +29,13 @@ export const Default = () => html`<a
 >
   Legacy docs <span is="vl-icon" data-vl-icon="external" data-vl-after data-vl-link></span
 ></a>`;
+
+export const Test = () => {
+  return html`
+    <select id="multiselect" is="vl-multiselect">
+      <option value="Belgium">België</option>
+      <option value="Germany">Duitsland</option>
+      <option value="France">Frankrijk</option>
+    </select>
+  `;
+};
