877e08872dbb7f7b3c81369fec72ce9778414004
Gert-Jan Meire <gertjan.meire@gmail.com>
2022-08-10 14:47:38 +0200

    UIG-2110 - Added side navigation.

---
+++ nx/libs/elements/src/lib/side-navigation/components/vl-side-navigation-content.stories.ts
@@ -0,0 +1,10 @@
+import "../vl-side-navigation.element";
+
+export default {
+  title: "native-elements/vl-side-navigation/vl-side-navigation-content",
+  parameters: {
+    controls: { hideNoControlsWarning: true },
+  },
+};
+
+export { Default } from "../vl-side-navigation.stories";
---
+++ nx/libs/elements/src/lib/side-navigation/components/vl-side-navigation-h3.stories.ts
@@ -0,0 +1,10 @@
+import "../vl-side-navigation.element";
+
+export default {
+  title: "native-elements/vl-side-navigation/vl-side-navigation-h3",
+  parameters: {
+    controls: { hideNoControlsWarning: true },
+  },
+};
+
+export { Default } from "../vl-side-navigation.stories";
