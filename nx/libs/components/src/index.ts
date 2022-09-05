f1082e3f485d7ebb1a68a7204b74889a6a4d456c
Gert-Jan Meire <unknown>
2022-09-05 11:49:49 +0200

    UIG-2110 - Added datepicker component.

--- nx/libs/components/src/index.ts
+++ nx/libs/components/src/index.ts
@@ -4,6 +4,7 @@ export * from './lib/breadcrumb/vl-breadcrumb.component';
 export * from './lib/breadcrumb/vl-breadcrumb-item.component';
 export * from './lib/contact-card/vl-contact-card.component';
 export * from './lib/content-header/vl-content-header.component';
+export * from './lib/datepicker/vl-datepicker.component';
 export * from './lib/document/vl-document.component';
 export * from './lib/functional-header/vl-functional-header.component';
 export * from './lib/infoblock/vl-infoblock.component';
