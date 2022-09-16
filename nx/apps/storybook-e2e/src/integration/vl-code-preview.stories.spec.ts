55ee9d81762768b7d9e820e3c4f1a4884a6d1641
Kris Speltincx <kris.speltincx@vlaanderen.be>
2022-09-16 15:43:33 +0200

    nx - numbers document verifiëren / opkuis / aanpassingen / opmerkingen  -> JS Components = ok

--- nx/apps/storybook-e2e/src/integration/vl-code-preview.stories.spec.ts
+++ nx/apps/storybook-e2e/src/integration/vl-code-preview.stories.spec.ts
@@ -2,12 +2,11 @@ const codePreviewUrl = 'http://localhost:4600/iframe.html?id=components-vl-code-
 
 describe('story vl-code-preview', () => {
     it('should contain a code section', () => {
-        cy.visit(`${codePreviewUrl}`)
+        cy.visit(`${codePreviewUrl}`);
         cy.getDataCy('code-preview')
-        .shadow()
-        .find('code')
+            .shadow()
+            .find('code')
             .should('have.class', 'auto-indent')
-            .should('have.class', 'language-markup')
+            .should('have.class', 'language-markup');
     });
-
 });
