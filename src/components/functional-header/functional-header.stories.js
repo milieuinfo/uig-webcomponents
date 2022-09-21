1dcf671adbd5390229a35607e8ad3eb65d12f426
kodeco <kodeco@gmail.com>
2022-09-21 11:53:01 +0200

    OMGVN-8341, OMGVN-8403 en OMGVN-8401: Uitbreidingen van de functional header. Extra slots toegevoegd: top-left, top-right, sub-header en back-link.

--- src/components/functional-header/functional-header.stories.js
+++ src/components/functional-header/functional-header.stories.js
@@ -1,70 +1,136 @@
-import { html } from "lit-html";
-import "../functional-header";
-import { docsIntro } from "../../../.storybook/utils.js";
+import { html } from 'lit-html';
+import '../functional-header';
+import '../../legacy/tabs';
+import styles from './stories-styles.scss';
+import { CATEGORIES, docsIntro, stylesheet } from '../../../.storybook/utils.js';
 
 export default {
-  title: "custom-elements/vl-functional-header",
+  title: 'custom-elements/vl-functional-header',
+  decorators: [(story) => html`${stylesheet(styles)}${story()}`],
   parameters: {
     docs: {
       description: {
         component: docsIntro({
-          root: "functional-header",
+          root: 'functional-header',
           intro:
-            "Toont bovenaan de pagina generieke informatie zonder af te leiden zoals bijvoorgeeld titel, acties, tab navigatie of zoek input.",
+            'Toont bovenaan de pagina generieke informatie zonder af te leiden zoals bijvoorgeeld titel, acties, tab navigatie of zoek input.',
         }),
       },
     },
   },
   args: {
-    title: "School- en studietoelagen",
-    subTitle: "Voor lager, middelbaar en hoger onderwijs",
-    link: "#",
-    backLink: "#",
-    back: "Terug",
+    title: 'School- en studietoelagen',
+    subTitle: 'Voor lager, middelbaar en hoger onderwijs',
+    link: '#',
+    backLink: '#',
+    back: 'Terug',
   },
   argTypes: {
     title: {
-      name: "data-vl-title",
-      type: { summary: "string" },
-      description:
-        "Attribuut wordt gebruikt om de tekst van de titel te bepalen.",
+      name: 'data-vl-title',
+      type: { summary: 'string' },
+      description: 'Attribuut wordt gebruikt om de tekst van de titel te bepalen.',
       table: {
-        defaultValue: { summary: "" },
+        defaultValue: { summary: '' },
       },
     },
     subTitle: {
-      name: "data-vl-sub-title",
-      type: { summary: "string" },
-      description:
-        "Attribuut wordt gebruikt om de tekst van de sub titel te bepalen.",
+      name: 'data-vl-sub-title',
+      type: { summary: 'string' },
+      description: 'Attribuut wordt gebruikt om de tekst van de sub titel te bepalen.',
       table: {
-        defaultValue: { summary: "" },
+        defaultValue: { summary: '' },
       },
     },
     link: {
-      name: "data-vl-link",
-      type: { summary: "string" },
-      description:
-        "Attribuut wordt gebruikt om de link van de titel te bepalen.",
+      name: 'data-vl-link',
+      type: { summary: 'string' },
+      description: 'Attribuut wordt gebruikt om de link van de titel te bepalen.',
       table: {
-        defaultValue: { summary: "" },
+        defaultValue: { summary: '' },
       },
     },
     backLink: {
-      name: "data-vl-back-link",
-      type: { summary: "string" },
-      description: "Attribuut wordt gebruikt om de terug link te bepalen.",
+      name: 'data-vl-back-link',
+      type: { summary: 'string' },
+      description: 'Attribuut wordt gebruikt om de terug link te bepalen.',
       table: {
-        defaultValue: { summary: "" },
+        defaultValue: { summary: '' },
       },
     },
     back: {
-      name: "data-vl-back",
-      type: { summary: "string" },
-      description:
-        "Attribuut wordt gebruikt om de terug link tekst te bepalen.",
+      name: 'data-vl-back',
+      type: { summary: 'string' },
+      description: 'Attribuut wordt gebruikt om de terug link tekst te bepalen.',
       table: {
-        defaultValue: { summary: "" },
+        defaultValue: { summary: '' },
+      },
+    },
+    titleSlot: {
+      name: 'title',
+      description: 'Slot wordt gebruikt om de tekst van de titel te bepalen.',
+      table: {
+        category: CATEGORIES.SLOTS,
+        defaultValue: { summary: undefined },
+      },
+    },
+    subTitleSlot: {
+      name: 'sub-title',
+      description: 'Slot wordt gebruikt om de tekst van de sub titel te bepalen.',
+      table: {
+        category: CATEGORIES.SLOTS,
+        defaultValue: { summary: undefined },
+      },
+    },
+    backLinkSlot: {
+      name: 'back-link',
+      description:
+        'Defines the complete back link section that is displayed in the default sub header (section below the horizontal line)',
+      table: {
+        category: CATEGORIES.SLOTS,
+        defaultValue: { summary: undefined },
+      },
+    },
+    backSlot: {
+      name: 'back',
+      description: 'Slot wordt gebruikt om de terug link tekst te bepalen.',
+      table: {
+        category: CATEGORIES.SLOTS,
+        defaultValue: { summary: undefined },
+      },
+    },
+    actionsSlot: {
+      name: 'actions',
+      description:
+        'Defines what is displayed in the actions section in the top-left corner (right below the top-right slot) of the functional-header',
+      table: {
+        category: CATEGORIES.SLOTS,
+        defaultValue: { summary: undefined },
+      },
+    },
+    topLeftSlot: {
+      name: 'top-left',
+      description: 'Defines what is displayed in the top-left corner of the functional-header',
+      table: {
+        category: CATEGORIES.SLOTS,
+        defaultValue: { summary: undefined },
+      },
+    },
+    topRightSlot: {
+      name: 'top-right',
+      description: 'Defines what is displayed in the top-right corner of the functional-header',
+      table: {
+        category: CATEGORIES.SLOTS,
+        defaultValue: { summary: undefined },
+      },
+    },
+    subHeaderSlot: {
+      name: 'sub-header',
+      description:
+        'Defines what is displayed in the sub-header (section below the horizontal line). If this slot is defines, the sub-title slot and the data-vl-sub-title, data-vl-link, data-vl-back-link attributes are ignored.',
+      table: {
+        category: CATEGORIES.SLOTS,
+        defaultValue: { summary: undefined },
       },
     },
   },
@@ -80,11 +146,7 @@ export const Default = ({ title, subTitle, link, backLink, back }) =>
   ></vl-functional-header>`;
 
 export const WithSlotElements = ({ title, subTitle, link, backLink, back }) =>
-  html`<vl-functional-header
-    data-vl-link=${link}
-    data-vl-back-link=${backLink}
-    data-vl-back=${back}
-  >
+  html`<vl-functional-header data-vl-link=${link} data-vl-back-link=${backLink} data-vl-back=${back}>
     <span slot="title">${title}</span>
     <span slot="sub-title">${subTitle}</span>
     <span slot="back">${back}</span>
@@ -92,29 +154,18 @@ export const WithSlotElements = ({ title, subTitle, link, backLink, back }) =>
 
 WithSlotElements.argTypes = {
   title: {
-    name: "title (slot)",
+    name: 'title (slot)',
   },
   subTitle: {
-    name: "sub-title (slot)",
+    name: 'sub-title (slot)',
   },
   back: {
-    name: "back (slot)",
+    name: 'back (slot)',
   },
 };
 
-export const WithUserInteraction = ({
-  title,
-  subTitle,
-  link,
-  backLink,
-  back,
-  content,
-}) => html`
-  <vl-functional-header
-    data-vl-link=${link}
-    data-vl-back-link=${backLink}
-    data-vl-back=${back}
-  >
+export const WithUserInteraction = ({ title, subTitle, link, backLink, back, content }) => html`
+  <vl-functional-header data-vl-link=${link} data-vl-back-link=${backLink} data-vl-back=${back}>
     <span slot="title">${title}</span>
     <span slot="sub-title">${subTitle}</span>
     <span slot="back">${back}</span>
@@ -125,21 +176,68 @@ export const WithUserInteraction = ({
 `;
 
 WithUserInteraction.args = {
-  content: "Koen Peeters",
+  content: 'Koen Peeters',
 };
 
 WithUserInteraction.argTypes = {
   content: {
-    name: "actions (slot)",
-    type: "string",
+    name: 'actions (slot)',
+    type: 'string',
   },
   title: {
-    name: "title (slot)",
+    name: 'title (slot)',
   },
   subTitle: {
-    name: "sub-title (slot)",
+    name: 'sub-title (slot)',
   },
   back: {
-    name: "back (slot)",
+    name: 'back (slot)',
+  },
+};
+
+export const InzageFunctionalHeader = ({ title, topLeft, topRight, subHeader }) => html`
+  <vl-functional-header>
+    <span slot="top-left" class="super-title">${topLeft}</span>
+
+    <span slot="title">${title}</span>
+
+    <div slot="top-right">${topRight}</div>
+
+    <span slot="sub-header">${subHeader}</span>
+  </vl-functional-header>
+`;
+
+InzageFunctionalHeader.args = {
+  title: 'Project overzicht',
+  topLeft: html`<h2 class="vl-title vl-title--h2">
+    2458963498
+    <span class="vl-annotation vl-annotation--small">(MILIEUBEDRIJF - RINGVAART - OCGT)</span>
+  </h2>`,
+  topRight: html`<span>Toestand: In beroepsperiode tot 01.02.2023</span>
+    <a href="#">Ik wens een beroepschrift aan te maken</a>
+    <span>Project navigatie</span>`,
+  subHeader: html`<a id="back-link" is="vl-link" href="${document.referrer}" tabindex="0">
+      <span is="vl-icon" data-vl-icon="arrow-left-fat" data-vl-before></span>
+      <slot id="back-link-text" name="back">
+        <span>Eén stap terug</span>
+      </slot>
+    </a>
+    <span>Project inhoud</span>
+    <a href="#">Project information</a>
+    <a href="#">Project verloop</a>`,
+};
+
+InzageFunctionalHeader.argTypes = {
+  title: {
+    name: 'title (slot)',
+  },
+  topLeft: {
+    name: 'top-left (slot)',
+  },
+  topRight: {
+    name: 'top-right (slot)',
+  },
+  subHeader: {
+    name: 'sub-header (slot)',
   },
 };
--- src/components/functional-header/test/e2e/functional-header.spec.js
+++ src/components/functional-header/test/e2e/functional-header.spec.js
@@ -1,10 +1,11 @@
-import { assert, getDriver, config } from '../../../../utils/test';
+import { By, assert, getDriver, config } from '../../../../utils/test';
 import { VlFunctionalHeader } from './functional-header';
 
 const { sbUrl } = config;
 const defaultUrl = `${sbUrl}?id=custom-elements-vl-functional-header--default`;
 const withSlotsUrl = `${sbUrl}?id=custom-elements-vl-functional-header--with-slot-elements`;
 const withActionsUrl = `${sbUrl}?id=custom-elements-vl-functional-header--with-user-interaction`;
+const withCustomSubHeaderUrl = `${sbUrl}?id=custom-elements-vl-functional-header--inzage-functional-header`;
 const selector = 'vl-functional-header';
 
 describe('vl-functional-header', async () => {
@@ -21,17 +22,31 @@ describe('vl-functional-header', async () => {
     const functionalHeader = await new VlFunctionalHeader(driver, selector);
     const title = await functionalHeader.getTitle();
     const subTitle = await functionalHeader.getSubTitle();
+    const defaultSubHeader = await functionalHeader.getDefaultSubHeader();
     await assert.eventually.equal(title.getText(), 'SCHOOL- EN STUDIETOELAGEN');
     await assert.eventually.equal(subTitle.getText(), 'Voor lager, middelbaar en hoger onderwijs');
+    await assert.eventually.equal(defaultSubHeader.getAttribute('hidden'), null);
+
+    const topLeftSlotNodes = await functionalHeader.getTopLeftSlotNodes();
+    await assert.equal(topLeftSlotNodes.length, 0);
+    const topRightSlotNodes = await functionalHeader.getTopRightSlotNodes();
+    await assert.equal(topRightSlotNodes.length, 0);
   });
 
-  it('as a user, I can the title and sub title of the functional header set via a slot', async () => {
+  it('as a user, I can see the title and sub title of the functional header set via a slot', async () => {
     await driver.get(withSlotsUrl);
     const functionalHeader = await new VlFunctionalHeader(driver, selector);
     const titleSlotNodes = await functionalHeader.getTitleSlotNodes();
     const subTitleSlotNodes = await functionalHeader.getSubTitleSlotNodes();
+    const defaultSubHeader = await functionalHeader.getDefaultSubHeader();
     await assert.eventually.equal(titleSlotNodes[0].getText(), 'SCHOOL- EN STUDIETOELAGEN');
     await assert.eventually.equal(subTitleSlotNodes[0].getText(), 'Voor lager, middelbaar en hoger onderwijs');
+    await assert.eventually.equal(defaultSubHeader.getAttribute('hidden'), null);
+
+    const topLeftSlotNodes = await functionalHeader.getTopLeftSlotNodes();
+    await assert.equal(topLeftSlotNodes.length, 0);
+    const topRightSlotNodes = await functionalHeader.getTopRightSlotNodes();
+    await assert.equal(topRightSlotNodes.length, 0);
   });
 
   it('as a user, I can click on the title link', async () => {
@@ -42,6 +57,14 @@ describe('vl-functional-header', async () => {
     await title.click();
     await assert.eventually.isFalse(getCurrentUrl().then((url) => url.endsWith('&viewMode=story')));
     await assert.eventually.isTrue(getCurrentUrl().then((url) => url.endsWith('#')));
+
+    const defaultSubHeader = await functionalHeader.getDefaultSubHeader();
+    await assert.eventually.equal(defaultSubHeader.getAttribute('hidden'), null);
+
+    const topLeftSlotNodes = await functionalHeader.getTopLeftSlotNodes();
+    await assert.equal(topLeftSlotNodes.length, 0);
+    const topRightSlotNodes = await functionalHeader.getTopRightSlotNodes();
+    await assert.equal(topRightSlotNodes.length, 0);
   });
 
   it('as a user, I can click on an action', async () => {
@@ -50,5 +73,51 @@ describe('vl-functional-header', async () => {
     const actions = await functionalHeader.getActionNodes();
     await actions[0].click();
     await assert.eventually.isTrue(getCurrentUrl().then((url) => url.endsWith('#')));
+
+    const defaultSubHeader = await functionalHeader.getDefaultSubHeader();
+    await assert.eventually.equal(defaultSubHeader.getAttribute('hidden'), null);
+
+    const topLeftSlotNodes = await functionalHeader.getTopLeftSlotNodes();
+    await assert.equal(topLeftSlotNodes.length, 0);
+    const topRightSlotNodes = await functionalHeader.getTopRightSlotNodes();
+    await assert.equal(topRightSlotNodes.length, 0);
+  });
+
+  it('as a user, I see the custom sub header and the default sub header is hidden', async () => {
+    await driver.get(withCustomSubHeaderUrl);
+    const functionalHeader = await new VlFunctionalHeader(driver, selector);
+    const titleSlotNodes = await functionalHeader.getTitleSlotNodes();
+    const subHeaderSlotNodes = await functionalHeader.getSubHeaderSlotNodes();
+    await assert.eventually.equal(titleSlotNodes[0].getText(), 'PROJECT OVERZICHT');
+
+    const backLinkText = await subHeaderSlotNodes[0].findElement(By.css('#back-link-text'));
+    await assert.eventually.equal(backLinkText.getText(), 'Eén stap terug');
+    await assert.eventually.equal(subHeaderSlotNodes[1].getText(), 'Project inhoud');
+    await assert.eventually.equal(subHeaderSlotNodes[2].getText(), 'Project information');
+    await assert.eventually.equal(subHeaderSlotNodes[3].getText(), 'Project verloop');
+
+    const defaultSubHeader = await functionalHeader.getDefaultSubHeader();
+    await assert.eventually.equal(defaultSubHeader.getAttribute('hidden'), 'true');
+
+    const topLeftSlotNodes = await functionalHeader.getTopLeftSlotNodes();
+    await assert.eventually.equal(topLeftSlotNodes[0].getText(), '2458963498 (MILIEUBEDRIJF - RINGVAART - OCGT)');
+
+    const topRightSlotNodes = await functionalHeader.getTopRightSlotNodes();
+    await assert.eventually.equal(topRightSlotNodes[0].getText(), 'Toestand: In beroepsperiode tot 01.02.2023');
+    await assert.eventually.equal(topRightSlotNodes[1].getText(), 'Ik wens een beroepschrift aan te maken');
+    await assert.eventually.equal(topRightSlotNodes[2].getText(), 'Project navigatie');
+  });
+
+  it('as a user, I see the top-left and top-right slots filled with data', async () => {
+    await driver.get(withCustomSubHeaderUrl);
+    const functionalHeader = await new VlFunctionalHeader(driver, selector);
+
+    const topLeftSlotNodes = await functionalHeader.getTopLeftSlotNodes();
+    await assert.eventually.equal(topLeftSlotNodes[0].getText(), '2458963498 (MILIEUBEDRIJF - RINGVAART - OCGT)');
+
+    const topRightSlotNodes = await functionalHeader.getTopRightSlotNodes();
+    await assert.eventually.equal(topRightSlotNodes[0].getText(), 'Toestand: In beroepsperiode tot 01.02.2023');
+    await assert.eventually.equal(topRightSlotNodes[1].getText(), 'Ik wens een beroepschrift aan te maken');
+    await assert.eventually.equal(topRightSlotNodes[2].getText(), 'Project navigatie');
   });
 });
